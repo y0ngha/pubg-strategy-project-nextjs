ca8e405b0c017ba40f5301ebfb0973a3bb77fdcf
y0ngha <dev.y0ngha@gmail.com>
2025-12-11 22:01:54 +0900

    test: DI Auto Inject 테스트 코드 작성

---
+++ app/__tests__/global/di/di.spec.ts
@@ -0,0 +1,88 @@
+import { Container } from 'inversify';
+import { autoDependenciesBindForClass, autoDependenciesBindForValue } from '../../../global/di/helpers/auto-inject';
+import { ClassDependency, ValueDependency } from '../../../global/di/types/di-types';
+
+describe('InversifyJS Dependency Injection Setup', () => {
+    let container: Container;
+
+    class TestService {
+        isService: boolean = true;
+    }
+
+    const dataSourceInstance = new Map<number, string>();
+
+    const configValue = "production_key_12345";
+
+    type DISymbol = { [symbolKey: string]: symbol }
+
+    const DISymbols: DISymbol = {
+        TestService: Symbol.for("TestService"),
+        DataSource: Symbol.for("DataSource"),
+        Config: Symbol.for("Config")
+    }
+
+    const dependencyInjectedClasses: ClassDependency = {
+        TestService: { symbol: DISymbols.TestService, class: TestService },
+    } as const;
+
+    const dependencyInjectedValues: ValueDependency = {
+        DataSource: { symbol: DISymbols.DataSource, value: dataSourceInstance },
+        Config: { symbol: DISymbols.Config, value: configValue },
+    } as const;
+
+    beforeEach(() => {
+        container = new Container();
+
+        autoDependenciesBindForClass(dependencyInjectedClasses, container);
+        autoDependenciesBindForValue(dependencyInjectedValues, container);
+
+        expect(container).toBeInstanceOf(Container);
+    });
+
+    describe('Class Bindings (.to(Class)) 테스트', () => {
+        it('TestService를 Container에서 가져오고, 이는 타입이 동일해야합니다.', () => {
+            const instance = container.get<TestService>(DISymbols.TestService);
+
+            expect(instance).toBeDefined();
+            expect(instance).toBeInstanceOf(TestService);
+        });
+
+        it('TestService를 Container에서 두번 가져오고, 처음 가져온 Instance와 다음으로 가져온 Instance가 동일해야합니다(Singleton).', () => {
+            const instance = container.get<TestService>(DISymbols.TestService);
+            const secondInstance = container.get<TestService>(DISymbols.TestService);
+
+            expect(instance).toBe(secondInstance);
+            expect(instance.isService).toBe(true);
+        })
+    });
+
+    describe('Value Bindings (.toConstantValue(Value)) 테스트', () => {
+
+        it('Datasource를 Container에서 Map 타입으로 가져오고, 이는 타입과 값이 엄격하게 동일해야합니다.', () => {
+            const value = container.get<Map<number, string>>(DISymbols.DataSource);
+
+            expect(value).toBeDefined();
+            expect(value).toBeInstanceOf(Map);
+            expect(value).toBe(dataSourceInstance);
+        });
+
+        it('Config를 Container에서 String 타입으로 가져오고, 이는 타입과 값이 엄격하게 동일해야합니다.', () => {
+            const value = container.get<string>(DISymbols.Config);
+
+            expect(value).toBeDefined();
+            expect(value).toBe(configValue);
+            expect(value).toBe(dependencyInjectedValues.Config.value);
+        });
+    });
+
+
+    describe('Failure Test (Unbound Symbols)', () => {
+        it('존재하지 않는 심볼을 가져올 때 오류가 발생합니다.', () => {
+            const UNBOUND_SYMBOL = Symbol.for("UnboundSymbol");
+
+            expect(() => {
+                container.get(UNBOUND_SYMBOL);
+            }).toThrow();
+        });
+    });
+});
