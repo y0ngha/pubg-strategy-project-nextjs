3674315749265aadda8cd4a560c8ddbbaf57a7ed
y0ngha <dev.y0ngha@gmail.com>
2025-12-12 03:09:07 +0900

    refactor: auto-inject 코드 효율적으로 변경

--- app/global/di/helpers/auto-inject.ts
+++ app/global/di/helpers/auto-inject.ts
@@ -5,25 +5,21 @@ export function autoDependenciesBindForClass(
     classes: ClassDependency,
     container: Container
 ): void {
-    (Object.keys(classes))
-        .forEach(classKey => {
-            const classDefinition = classes[classKey];
-            container
-                .bind<globalThis.InstanceType<typeof classDefinition.class>>(classDefinition.symbol)
-                .to(classDefinition.class)
-                .inSingletonScope();
-        });
+    for (const classDefinition of Object.values(classes)) {
+        container
+            .bind<globalThis.InstanceType<typeof classDefinition.class>>(classDefinition.symbol)
+            .to(classDefinition.class)
+            .inSingletonScope();
+    }
 }
 
 export function autoDependenciesBindForValue(
     values: ValueDependency,
     container: Container
 ): void {
-    (Object.keys(values))
-        .forEach(valueKey => {
-            const valueDefinition = values[valueKey];
-            container
-                .bind<typeof valueDefinition.value>(valueDefinition.symbol)
-                .toConstantValue(valueDefinition.value)
-        });
+    for (const valueDefinition of Object.values(values)) {
+        container
+            .bind<typeof valueDefinition.value>(valueDefinition.symbol)
+            .toConstantValue(valueDefinition.value)
+    }
 }
