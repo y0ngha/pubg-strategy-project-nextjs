2d247090ceedfe752323c7e7352605244729a020
y0ngha <dev.y0ngha@gmail.com>
2025-12-12 17:25:48 +0900

    refactor: di-type중 any > unknown으로 변경

--- app/global/di/types/di-types.ts
+++ app/global/di/types/di-types.ts
@@ -1,5 +1,5 @@
 import { Newable } from "inversify";
 
-export type ConstructorType = new (...args: any[]) => any;
-export type ClassDependency = { [symbolKey: string]: { symbol: symbol, class: Newable<any> } }
-export type ValueDependency = { [symbolKey: string]: { symbol: symbol, value: any } } 
+export type ConstructorType = new (...args: unknown[]) => unknown;
+export type ClassDependency = { [symbolKey: string]: { symbol: symbol, class: Newable<unknown> } }
+export type ValueDependency = { [symbolKey: string]: { symbol: symbol, value: unknown } } 
