d880ced4d8e582e9ca48731401c0a531c3ee7d0f
y0ngha <dev.y0ngha@gmail.com>
2025-12-13 22:15:13 +0900

    feat: api client에 들어갈 interceptors 정의

---
+++ app/infrastructure/http/interceptors.ts
@@ -0,0 +1,32 @@
+import { cookies } from 'next/headers';
+import { env } from '../config/environment-variables';
+import { RequestInterceptor, ResponseInterceptor } from './types';
+
+export const loggingRequestInterceptor: RequestInterceptor = async (url, config) => {
+    if (env.NODE_ENV === 'development') {
+        console.log(`[API Request] ${config.method} ${url}`);
+        console.log('[API Body]', config.body);
+    }
+    return { url, config };
+};
+
+export const loggingResponseInterceptor: ResponseInterceptor = (response) => {
+    if (env.NODE_ENV === 'development') {
+        console.log(`[API Response] ${response.status} ${response.url}`);
+    }
+    return response;
+};
+
+export const authRequestInterceptor: RequestInterceptor = async (url, config) => {
+    if (config.withAuthorizationHeader) {
+        const token = (await cookies()).get('auth_token')?.value;
+        if (token) {
+            config.headers = {
+                ...config.headers,
+                'Authorization': `Bearer ${token}`,
+            };
+        }
+    }
+
+    return { url, config };
+};
