a35e7773f7a59ded0cdf15b42c63d872e5e3269d
y0ngha <dev.y0ngha@gmail.com>
2025-12-15 18:07:37 +0900

    style: prettier 적용

--- jest.config.js
+++ jest.config.js
@@ -1,30 +1,26 @@
 module.exports = {
-  cacheDirectory: './.jest/cache',
-  testEnvironment: 'jest-fixed-jsdom',
-  setupFilesAfterEnv: [
-    '<rootDir>/app/__tests__/infrastructure/http/api-client.setup.ts',
-  ],
-  testMatch: [
-    '**/__tests__/**/*.+(test|spec).+(ts|tsx|js)',
-  ],
-  transform: {
-    '^.+\\.(t|j)sx?$': [
-      'ts-jest',
-      {
-        tsconfig: 'tsconfig.json',
-      },
-    ]
-  },
-  // MSW를 위한 무시 처리
-  transformIgnorePatterns: [
-    'node_modules/(?!(msw|@msw|until-async)/)'
-  ],
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/app/$1',
-    '^@infrastructure/(.*)$': '<rootDir>/app/infrastructure/$1',
-    '^@global/(.*)$': '<rootDir>/app/global/$1',
-  },
-  testEnvironmentOptions: {
-    customExportConditions: [''],
-  },
-}
+    cacheDirectory: './.jest/cache',
+    testEnvironment: 'jest-fixed-jsdom',
+    setupFilesAfterEnv: [
+        '<rootDir>/app/__tests__/infrastructure/http/api-client.setup.ts',
+    ],
+    testMatch: ['**/__tests__/**/*.+(test|spec).+(ts|tsx|js)'],
+    transform: {
+        '^.+\\.(t|j)sx?$': [
+            'ts-jest',
+            {
+                tsconfig: 'tsconfig.json',
+            },
+        ],
+    },
+    // MSW를 위한 무시 처리
+    transformIgnorePatterns: ['node_modules/(?!(msw|@msw|until-async)/)'],
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/app/$1',
+        '^@infrastructure/(.*)$': '<rootDir>/app/infrastructure/$1',
+        '^@global/(.*)$': '<rootDir>/app/global/$1',
+    },
+    testEnvironmentOptions: {
+        customExportConditions: [''],
+    },
+};
