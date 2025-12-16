a207a1e39c60986ea5258e4405e5a735739340f1
y0ngha <dev.y0ngha@gmail.com>
2025-12-16 18:14:22 +0900

    rename: 폴더명 오타 수정

--- app/__tests__/domain/user/entities/user.entity.spec.ts
+++ app/__tests__/domain/user/entities/user.entity.spec.ts
@@ -2,7 +2,7 @@
 
 import { Email } from '@/domain/shared/value-objects/email';
 import { UserId } from '@/domain/shared/value-objects/user-id';
-import { User } from '@/domain/user/entitites/user.entity';
+import { User } from '@/domain/user/entities/user.entity';
 import { AuthProvider } from '@/domain/user/enums/AuthProvider.enum';
 import { Password } from '@/domain/user/value-objects/password';
 
