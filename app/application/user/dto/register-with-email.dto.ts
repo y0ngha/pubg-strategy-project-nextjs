b58f578c36f3dcdefec5eacce326cbd7d92465f1
y0ngha <dev.y0ngha@gmail.com>
2025-12-16 20:06:12 +0900

    feat: 이메일 회원가입 요청 DTO 생성

---
+++ app/application/user/dto/register-with-email.dto.ts
@@ -0,0 +1,16 @@
+import { Email } from '@/domain/shared/value-objects/email';
+import { Password } from '@/domain/user/value-objects/password';
+import { z } from 'zod';
+
+export const RegisterWithEmailRequestSchema = z.object({
+    email: z.email('유효하지 않은 이메일 형식입니다.').transform(value => {
+        return Email.create(value);
+    }),
+    password: z.string().transform(value => {
+        return Password.create(value);
+    }),
+});
+
+export type RegisterWithEmailRequestObject = z.infer<
+    typeof RegisterWithEmailRequestSchema
+>;
