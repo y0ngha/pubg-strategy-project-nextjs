a8e08fe5cb8a0df8af399d516e63057a248ef56d
y0ngha <dev.y0ngha@gmail.com>
2025-12-19 18:26:45 +0900

    test: 테스트 격리 후 테스트코드 재작성

--- app/__tests__/infrastructure/user/adapter/user-session.adapter.spec.ts
+++ app/__tests__/infrastructure/user/adapter/user-session.adapter.spec.ts
@@ -6,7 +6,7 @@ import { Password } from '@/domain/user/value-objects/password';
 import { UserSessionAdapter } from '@/infrastructure/user/adapter/user-session.adapter';
 
 describe('UserSessionAdapter (Client)', () => {
-    let userSessionPort: UserSessionPort
+    let userSessionPort: UserSessionPort;
 
     beforeEach(() => {
         userSessionPort = new UserSessionAdapter();
@@ -37,8 +37,19 @@ describe('UserSessionAdapter (Client)', () => {
     describe('save', () => {
         it('파라미터로 전달된 유저의 정보를 그대로 저장한다.', () => {
             // Give
-            const oldUser = userSessionPort.getUser();
+            const oldUser = User.createWithEmail(
+                Email.create('old@example.com'),
+                Password.create('Qwer1234@')
+            );
 
+            // When
+            userSessionPort.saveUser(oldUser);
+
+            // Then
+            const oldUserResponse = userSessionPort.getUser();
+            expect(oldUserResponse).toBe(oldUser);
+
+            // Give
             const newUser = User.createWithEmail(
                 Email.create('update@example.com'),
                 Password.create('Asdf1234@')
@@ -48,16 +59,34 @@ describe('UserSessionAdapter (Client)', () => {
             userSessionPort.saveUser(newUser);
 
             // Then
-            const updatedUser = userSessionPort.getUser();
+            const newUserResponse = userSessionPort.getUser();
 
-            expect(updatedUser).not.toBe(oldUser);
+            expect(newUserResponse).not.toBe(oldUser);
+            expect(newUserResponse).toBe(newUser);
         });
     });
 
     describe('clear', () => {
         it('호출시 저장된 User 정보에 상관 없이 삭제처리 되어야 한다.', () => {
+            // When
             userSessionPort.clearUser();
 
+            // Then
+            expect(() => userSessionPort.getUser()).toThrow(
+                new UserNotFoundException()
+            );
+
+            // Give
+            const user = User.createWithEmail(
+                Email.create('test@test.com'),
+                Password.create('Qwer1234@')
+            );
+            userSessionPort.saveUser(user);
+
+            // When
+            userSessionPort.clearUser();
+
+            // Then
             expect(() => userSessionPort.getUser()).toThrow(
                 new UserNotFoundException()
             );
