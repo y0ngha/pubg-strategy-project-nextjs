9a6c86731aa93432ebf4e5e3a3259a4e4e62fdf6
y0ngha <dev.y0ngha@gmail.com>
2025-12-30 21:26:42 +0900

    feat: google-auth-service 추상체 작성

---
+++ app/infrastructure/user/adapter/driven/google-auth-service.adapter.ts
@@ -0,0 +1,7 @@
+import { GoogleAuthServicePort } from '@/domain/user/port/out/google-auth-service.port';
+
+export class GoogleAuthServiceAdapter extends GoogleAuthServicePort {
+    getToken(): Promise<void> {
+        throw new Error('Not Implemented.');
+    }
+}
