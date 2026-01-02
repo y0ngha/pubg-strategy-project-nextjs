c385ba03f48ca3b1b2af815c2d5718e5376ab5b7
y0ngha <dev.y0ngha@gmail.com>
2026-01-02 16:12:46 +0900

    feat: 이메일 회원가입 Server Actions 작성

---
+++ app/presentation/user/actions/register-with-email.action.ts
@@ -0,0 +1,30 @@
+'use server';
+
+import { initializeRequestServices } from '@global/di/server/get-server-dependency';
+import { RegisterWithEmailUseCase } from '@/application/user/use-cases/register-with-email.usecase';
+
+export async function registerWithEmailAction(_: unknown, formData: FormData) {
+    const getService = initializeRequestServices();
+
+    const email = formData.get('email')?.toString();
+    const password = formData.get('password')?.toString();
+
+    if (email === undefined) {
+        throw new Error('이메일은 필수적으로 입력해야합니다.');
+    }
+
+    if (password === undefined) {
+        throw new Error('비밀번호는 필수적으로 입력해야합니다.');
+    }
+
+    const dto = {
+        email: email,
+        password: password,
+    };
+
+    const useCase = getService<RegisterWithEmailUseCase>(
+        RegisterWithEmailUseCase
+    );
+
+    return await useCase.execute(dto);
+}
