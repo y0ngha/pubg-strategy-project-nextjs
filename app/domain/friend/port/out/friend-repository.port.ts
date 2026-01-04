cb08d0c9774bab84a9c72bab53bfa768928bf28e
y0ngha <dev.y0ngha@gmail.com>
2026-01-04 18:03:15 +0900

    feat: friend-repository.port 정의

---
+++ app/domain/friend/port/out/friend-repository.port.ts
@@ -0,0 +1,19 @@
+import { Friend } from '@domain/friend/entities/friend.entity';
+import { UserId } from '@domain/shared/value-objects/user-id';
+import { FriendId } from '@domain/friend/value-objects/friend-id';
+
+export abstract class FriendRepositoryPort {
+    abstract save(friendship: Friend): Promise<void>;
+
+    abstract delete(id: UserId): Promise<void>;
+
+    abstract findById(id: FriendId): Promise<Friend | null>;
+
+    abstract findByUserId(id: UserId): Promise<Friend | null>;
+
+    abstract findFriendsByUserId(userId: UserId): Promise<Friend[]>;
+
+    abstract findReceivedRequests(userId: UserId): Promise<Friend[]>;
+
+    abstract findSentRequests(userId: UserId): Promise<Friend[]>;
+}
