dbd33a285ae60eaa44332c9fc9fb43807a0d4829
y0ngha <dev.y0ngha@gmail.com>
2026-01-04 19:16:30 +0900

    test: 친구 상태중 `CANCEL` 테스트 케이스 추가

--- app/__tests__/application/friend/use-cases/accept-friendship.usecase.spec.ts
+++ app/__tests__/application/friend/use-cases/accept-friendship.usecase.spec.ts
@@ -28,7 +28,7 @@ describe('AcceptFriendshipUseCase', () => {
     });
 
     describe('성공 테스트', () => {
-        it('요청 받은 친구 관계가 존재하고, 받은 사람이 본인이며, 친구 상태가 PENDING일 때 성공한다.', async () => {
+        it('친구 관계가 존재하고, 받은 사람이 본인이며, 친구 상태가 PENDING일 때 성공한다.', async () => {
             // give
             const requesterUserId = 'a0f01e35-f96b-4dee-a75b-89cea500ce50';
             const recipientUserId = 'c92c6abc-82aa-423f-83e9-9e2823c1bcf7';
@@ -65,8 +65,9 @@ describe('AcceptFriendshipUseCase', () => {
             expect(savedFriend.respondedAt).not.toBeNull();
         });
     });
+
     describe('실패 테스트', () => {
-        it('요청 받은 친구 관계가 없을 때 에러를 던진다.', async () => {
+        it('친구 관계가 없을 때 에러를 던진다.', async () => {
             // give
             const recipientUserId = 'c92c6abc-82aa-423f-83e9-9e2823c1bcf7';
 
@@ -189,6 +190,42 @@ describe('AcceptFriendshipUseCase', () => {
             // when & then
             await expect(useCase.execute(dto)).rejects.toThrow(
                 new FriendshipUpdateInvalidStatus(FriendshipStatus.REJECTED)
+            );
+
+            expect(mockFriendRepository.findById).toHaveBeenCalledTimes(1);
+            expect(mockFriendRepository.save).toHaveBeenCalledTimes(0);
+        });
+
+        it('요청 받은 친구 관계가 취소된 상태인 경우 에러를 던진다.', async () => {
+            // give
+            const requesterUserId = 'a0f01e35-f96b-4dee-a75b-89cea500ce50';
+            const recipientUserId = 'c92c6abc-82aa-423f-83e9-9e2823c1bcf7';
+
+            const requesterUserEmail = 'requester@domain.com';
+            const recipientUserEmail = 'recipient@domain.com';
+
+            mockFriendRepository.findById.mockImplementation(
+                async (): Promise<Friend | null> => {
+                    const friend = Friend.create(
+                        UserId.create(requesterUserId),
+                        UserId.create(recipientUserId),
+                        Email.create(requesterUserEmail),
+                        Email.create(recipientUserEmail)
+                    );
+                    friend.cancel(UserId.create(requesterUserId));
+
+                    return friend;
+                }
+            );
+
+            const dto = {
+                id: '869215ed-3baf-4abb-a511-b164f0cc716e',
+                userId: recipientUserId,
+            };
+
+            // when & then
+            await expect(useCase.execute(dto)).rejects.toThrow(
+                new FriendshipUpdateInvalidStatus(FriendshipStatus.CANCELED)
             );
 
             expect(mockFriendRepository.findById).toHaveBeenCalledTimes(1);
