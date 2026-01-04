b784cdf465993e3531e97c5962c22349ae752cbc
y0ngha <dev.y0ngha@gmail.com>
2026-01-04 21:57:00 +0900

    feat: friend repository 임시 구현

---
+++ app/infrastructure/friend/adapter/driven/friend-repository.adatper.ts
@@ -0,0 +1,41 @@
+import { FriendRepositoryPort } from '@domain/friend/port/out/friend-repository.port';
+import { FriendId } from '@domain/friend/value-objects/friend-id';
+import { UserId } from '@domain/shared/value-objects/user-id';
+import { Friend } from '@domain/friend/entities/friend.entity';
+
+export class FriendRepositoryAdatper extends FriendRepositoryPort {
+    delete(id: FriendId): Promise<void> {
+        throw new Error('Not Implemented.');
+    }
+
+    existsFriendshipBetween(
+        userId1: UserId,
+        userId2: UserId
+    ): Promise<boolean> {
+        throw new Error('Not Implemented.');
+    }
+
+    findAcceptedFriendsByUserId(userId: UserId): Promise<Friend[]> {
+        throw new Error('Not Implemented.');
+    }
+
+    findById(id: FriendId): Promise<Friend | null> {
+        throw new Error('Not Implemented.');
+    }
+
+    findReceivedFriendshipRequestsByRecipientUserId(
+        userId: UserId
+    ): Promise<Friend[]> {
+        throw new Error('Not Implemented.');
+    }
+
+    findSentFriendshipRequestsByRequesterUserId(
+        userId: UserId
+    ): Promise<Friend[]> {
+        throw new Error('Not Implemented.');
+    }
+
+    save(friend: Friend): Promise<void> {
+        throw new Error('Not Implemented.');
+    }
+}
