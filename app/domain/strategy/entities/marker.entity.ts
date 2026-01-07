6ac4ec7fda3514a41ddb8f3915245d62302fa9a7
y0ngha <dev.y0ngha@gmail.com>
2026-01-07 22:08:45 +0900

    feat: Marker Entity 작성 (#88)
    * feat: marker entity 작성
    
    * refactor: updateAt 오타 수정
    
    * refactor: deletedAt 삭제.
    - 이를 영속성으로 저장하려고 하지 않으니 삭제가 된거면 삭제가 된거기 때문에 의미 없는 데이터.
    
    * refactor: 클래스 응집력 개선 및 로직 가독성 개선

---
+++ app/domain/strategy/entities/marker.entity.ts
@@ -0,0 +1,64 @@
+import { MarkerId } from '@domain/strategy/value-objects/marker-id';
+import { Position } from '@domain/strategy/value-objects/position';
+
+export class Marker {
+    private constructor(
+        public readonly id: MarkerId,
+        private _position: Position,
+        private _isDeleted: boolean,
+        public readonly createdAt: Date,
+        private _updatedAt: Date
+    ) {}
+
+    get position(): Position {
+        return this._position;
+    }
+
+    get updatedAt(): Date {
+        return this._updatedAt;
+    }
+
+    get isDeleted(): boolean {
+        return this._isDeleted;
+    }
+
+    static create(position: Position) {
+        return new Marker(
+            MarkerId.generate(),
+            position,
+            false,
+            new Date(),
+            new Date()
+        );
+    }
+
+    update(newPosition: Position) {
+        if (this._isDeleted) {
+            this.restore(newPosition);
+            return;
+        }
+
+        if (this._position.equals(newPosition)) {
+            this.delete();
+            return;
+        }
+
+        this.changePosition(newPosition);
+    }
+
+    delete() {
+        if (this._isDeleted) return;
+
+        this._isDeleted = true;
+    }
+
+    private restore(position: Position) {
+        this._isDeleted = false;
+        this.changePosition(position);
+    }
+
+    private changePosition(position: Position) {
+        this._position = position;
+        this._updatedAt = new Date();
+    }
+}
