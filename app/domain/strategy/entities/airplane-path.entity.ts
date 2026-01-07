91cd4b37c2257321f4a23a2c22478b151ca6788e
y0ngha <dev.y0ngha@gmail.com>
2026-01-07 22:34:43 +0900

    feat: 비행기 동선 Entity 작성

---
+++ app/domain/strategy/entities/airplane-path.entity.ts
@@ -0,0 +1,50 @@
+import { Position } from '@domain/strategy/value-objects/position';
+import { AirplanePathId } from '@domain/strategy/value-objects/airplane-path-id';
+import { AirplanePathCreateDuplicatePositionException } from '@domain/strategy/exceptions/strategy.exceptions';
+
+export class AirplanePath {
+    private constructor(
+        public readonly id: AirplanePathId,
+        public readonly startPosition: Position,
+        public readonly endPosition: Position,
+        private _isDeleted: boolean,
+        public readonly createdAt: Date
+    ) {
+        this.validateAirplanePath(startPosition, endPosition);
+    }
+
+    get isDeleted(): boolean {
+        return this._isDeleted;
+    }
+
+    static create(startPosition: Position, endPosition: Position) {
+        return new AirplanePath(
+            AirplanePathId.generate(),
+            startPosition,
+            endPosition,
+            false,
+            new Date()
+        );
+    }
+
+    delete() {
+        if (this._isDeleted) return;
+        this._isDeleted = true;
+    }
+
+    private validateAirplanePath(
+        startPosition: Position,
+        endPosition: Position
+    ) {
+        this.ensureDiffrentPosition(startPosition, endPosition);
+    }
+
+    private ensureDiffrentPosition(
+        startPosition: Position,
+        endPosition: Position
+    ) {
+        if (startPosition.equals(endPosition)) {
+            throw new AirplanePathCreateDuplicatePositionException();
+        }
+    }
+}
