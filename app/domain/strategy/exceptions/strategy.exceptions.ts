1229fcc9e78201e5d9b9f142584de7c9ef5bd3ae
y0ngha <dev.y0ngha@gmail.com>
2026-01-08 19:19:04 +0900

    feat: 전략에서 팀 플레이어에 대한 예외 클래스 정의

--- app/domain/strategy/exceptions/strategy.exceptions.ts
+++ app/domain/strategy/exceptions/strategy.exceptions.ts
@@ -23,3 +23,15 @@ export class AirplanePathCreateDuplicatePositionException extends DomainExceptio
         super('비행기의 시작과 끝이 동일합니다.');
     }
 }
+
+export class DeletedTeamPlayerException extends DomainException {
+    constructor() {
+        super('삭제된 플레이어에게는 할 수 없습니다.');
+    }
+}
+
+export class InvalidTeamPlayerPriorityException extends DomainException {
+    constructor() {
+        super('유효하지 않은 팀 플레이어 순번입니다.');
+    }
+}
