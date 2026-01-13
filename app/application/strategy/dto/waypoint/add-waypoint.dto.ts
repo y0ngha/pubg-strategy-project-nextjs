eae07986ce143882c25681d79fc8f164ac553c9a
y0ngha <dev.y0ngha@gmail.com>
2026-01-13 21:55:53 +0900

    feat: waypoint 관련 DTO 작성.

---
+++ app/application/strategy/dto/waypoint/add-waypoint.dto.ts
@@ -0,0 +1,34 @@
+import { z } from 'zod';
+import { UserId } from '@domain/shared/value-objects/user-id';
+import { StrategyId } from '@domain/strategy/value-objects/strategy-id';
+import { Position } from '@domain/strategy/value-objects/position';
+import { TeamPlayerId } from '@domain/strategy/value-objects/team-player-id';
+
+export interface AddWaypointRequestDto {
+    actorId: string;
+    strategyId: string;
+    teamPlayerId: string;
+    positions: { x: number; y: number }[];
+}
+
+export const AddWaypointRequestSchema = z.object({
+    actorId: z.string().transform(value => {
+        return UserId.create(value);
+    }),
+    strategyId: z.string().transform(value => {
+        return StrategyId.create(value);
+    }),
+    teamPlayerId: z.string().transform(value => {
+        return TeamPlayerId.create(value);
+    }),
+    positions: z
+        .array(
+            z.object({
+                x: z.number(),
+                y: z.number(),
+            })
+        )
+        .transform(values => {
+            return values.map(({ x, y }) => Position.create(x, y));
+        }),
+});
