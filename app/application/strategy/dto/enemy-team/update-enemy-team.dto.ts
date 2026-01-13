b83e064ce1bf305db25c3d9eca33b6db54354ec1
y0ngha <dev.y0ngha@gmail.com>
2026-01-13 21:40:17 +0900

    feat: 적 팀 관련 DTO 작성.

---
+++ app/application/strategy/dto/enemy-team/update-enemy-team.dto.ts
@@ -0,0 +1,36 @@
+import { z } from 'zod';
+import { UserId } from '@domain/shared/value-objects/user-id';
+import { StrategyId } from '@domain/strategy/value-objects/strategy-id';
+import { TeamLabel } from '@domain/strategy/value-objects/team-label';
+import { Position } from '@domain/strategy/value-objects/position';
+
+export interface UpdateEnemyTeamRequestDto {
+    actorId: string;
+    strategyId: string;
+    teamLabel?: string;
+    position?: { x: number; y: number };
+}
+
+export const UpdateEnemyTeamRequestSchema = z.object({
+    actorId: z.string().transform(value => {
+        return UserId.create(value);
+    }),
+    strategyId: z.string().transform(value => {
+        return StrategyId.create(value);
+    }),
+    teamLabel: z
+        .string()
+        .transform(value => {
+            return TeamLabel.create(value);
+        })
+        .optional(),
+    position: z
+        .object({
+            x: z.number(),
+            y: z.number(),
+        })
+        .transform(({ x, y }) => {
+            return Position.create(x, y);
+        })
+        .optional(),
+});
