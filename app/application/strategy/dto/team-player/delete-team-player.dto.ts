bf3574498ae0561e15e91274fdc861348fd36f6d
y0ngha <dev.y0ngha@gmail.com>
2026-01-13 21:53:37 +0900

    feat: 팀 플레이어 관련 DTO 작성.

---
+++ app/application/strategy/dto/team-player/delete-team-player.dto.ts
@@ -0,0 +1,22 @@
+import { z } from 'zod';
+import { UserId } from '@domain/shared/value-objects/user-id';
+import { StrategyId } from '@domain/strategy/value-objects/strategy-id';
+import { TeamPlayerId } from '@domain/strategy/value-objects/team-player-id';
+
+export interface DeleteTeamPlayerRequestDto {
+    actorId: string;
+    strategyId: string;
+    teamPlayerId: string;
+}
+
+export const DeleteTeamPlayerRequestSchema = z.object({
+    actorId: z.string().transform(value => {
+        return UserId.create(value);
+    }),
+    strategyId: z.string().transform(value => {
+        return StrategyId.create(value);
+    }),
+    teamPlayerId: z.string().transform(value => {
+        return TeamPlayerId.create(value);
+    }),
+});
