11f5237fb989307cea02b0ad0d2203207fa616bc
y0ngha <dev.y0ngha@gmail.com>
2026-01-14 18:25:51 +0900

    test: 태그 수정시 테스트 케이스 추가.

--- app/__tests__/application/strategy/use-cases/update-tag.usecase.spec.ts
+++ app/__tests__/application/strategy/use-cases/update-tag.usecase.spec.ts
@@ -9,6 +9,7 @@ import { StrategyId } from '@domain/strategy/value-objects/strategy-id';
 import { PubgMap } from '@domain/strategy/enums/map.enum';
 import { TagId } from '@domain/strategy/value-objects/tag-id';
 import { UpdateTagUseCase } from '@/application/strategy/use-cases/tag/update-tag.usecase';
+import { ZodError } from 'zod';
 
 describe('UpdateTagUseCase', () => {
     let useCase: UpdateTagUseCase;
@@ -111,5 +112,80 @@ describe('UpdateTagUseCase', () => {
 
         expect(tag?.content).toEqual(dto.content);
         expect(tag?.position).toEqual(dto.position);
+    });
+
+    it('태그 업데이트시 Content만 보낸다면, Content만 업데이트 된다.', async () => {
+        // given
+        mockStrategyRepository.findById.mockResolvedValue(strategyFixture);
+
+        const dto = {
+            actorId: ownerId.toString(),
+            strategyId: strategyId.toString(),
+            tagId: tagId.toString(),
+            content: '새로운 내용',
+        };
+
+        const tag = strategyFixture.tags.find(tag => tag.id.equals(tagId));
+
+        const oldPosition = tag?.position;
+        const oldConetnt = tag?.content;
+
+        // when
+        await useCase.execute(dto);
+
+        // then
+        expect(mockStrategyRepository.findById).toHaveBeenCalledTimes(1);
+        expect(mockStrategyRepository.save).toHaveBeenCalledTimes(1);
+
+        expect(tag?.content).not.toEqual(oldConetnt);
+        expect(tag?.content).toEqual(dto.content);
+        expect(tag?.position).toEqual(oldPosition);
+    });
+
+    it('태그 업데이트시 Position만 보낸다면, Position만 업데이트 된다.', async () => {
+        // given
+        mockStrategyRepository.findById.mockResolvedValue(strategyFixture);
+
+        const dto = {
+            actorId: ownerId.toString(),
+            strategyId: strategyId.toString(),
+            tagId: tagId.toString(),
+            position: {
+                x: 10,
+                y: 200,
+            },
+        };
+
+        const tag = strategyFixture.tags.find(tag => tag.id.equals(tagId));
+
+        const oldPosition = tag?.position;
+        const oldContent = tag?.content;
+
+        // when
+        await useCase.execute(dto);
+
+        // then
+        expect(mockStrategyRepository.findById).toHaveBeenCalledTimes(1);
+        expect(mockStrategyRepository.save).toHaveBeenCalledTimes(1);
+
+        expect(tag?.position).not.toEqual(oldPosition);
+        expect(tag?.position).toEqual(dto.position);
+        expect(tag?.content).toEqual(oldContent);
+    });
+
+    it('태그 업데이트시 업데이트할 속성을 보내지 않으면, 에러를 던진다.', async () => {
+        // given
+        const dto = {
+            actorId: ownerId.toString(),
+            strategyId: strategyId.toString(),
+            tagId: tagId.toString(),
+        };
+
+        // when & then
+        await expect(() => useCase.execute(dto)).rejects.toThrow(ZodError);
+
+        // then
+        expect(mockStrategyRepository.findById).toHaveBeenCalledTimes(0);
+        expect(mockStrategyRepository.save).toHaveBeenCalledTimes(0);
     });
 });
