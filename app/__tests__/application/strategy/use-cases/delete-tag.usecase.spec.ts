eaeec641f6528968b6b3eac1f7537ec015f39a15
y0ngha <dev.y0ngha@gmail.com>
2026-01-14 18:10:05 +0900

    test: 태그 삭제 테스트 케이스 작성.

---
+++ app/__tests__/application/strategy/use-cases/delete-tag.usecase.spec.ts
@@ -0,0 +1,99 @@
+import { StrategyRepositoryPort } from '@domain/strategy/port/out/strategy-repository.port';
+import {
+    StrategyNotFoundException,
+    TagNotFoundException,
+} from '@domain/strategy/exceptions/strategy.exceptions';
+import { Strategy } from '@domain/strategy/entities/strategy.entity';
+import { UserId } from '@domain/shared/value-objects/user-id';
+import { StrategyId } from '@domain/strategy/value-objects/strategy-id';
+import { PubgMap } from '@domain/strategy/enums/map.enum';
+import { DeleteTagUseCase } from '@/application/strategy/use-cases/tag/delete-tag.usecase';
+import { TagId } from '@domain/strategy/value-objects/tag-id';
+
+describe('DeleteTagUseCase', () => {
+    let useCase: DeleteTagUseCase;
+    let mockStrategyRepository: jest.Mocked<StrategyRepositoryPort>;
+    let strategyFixture: Strategy;
+
+    const ownerId = UserId.generate();
+
+    let strategyId: StrategyId;
+    let tagId: TagId;
+
+    const title = '전략 제목';
+    const map = PubgMap.ERANGEL;
+
+    beforeEach(() => {
+        mockStrategyRepository = {
+            save: jest.fn(),
+            delete: jest.fn(),
+            findById: jest.fn(),
+            findOwnedStrategiesByUserID: jest.fn(),
+            findSharedStrategiesByUserID: jest.fn(),
+        } as jest.Mocked<StrategyRepositoryPort>;
+
+        useCase = new DeleteTagUseCase(mockStrategyRepository);
+
+        strategyFixture = Strategy.create(ownerId, title, map);
+        strategyId = strategyFixture.id;
+
+        strategyFixture.addTag(ownerId, '삭제될 내용');
+        tagId = strategyFixture.tags[0].id;
+    });
+
+    it('전략을 찾지 못하면, 에러를 던진다.', async () => {
+        // given
+        mockStrategyRepository.findById.mockResolvedValue(null);
+        const dto = {
+            actorId: ownerId.toString(),
+            strategyId: strategyId.toString(),
+            tagId: tagId.toString(),
+        };
+
+        // when & then
+        await expect(() => useCase.execute(dto)).rejects.toThrow(
+            StrategyNotFoundException
+        );
+        expect(mockStrategyRepository.findById).toHaveBeenCalledTimes(1);
+    });
+
+    it('태그를 찾지 못하면, 에러를 던진다.', async () => {
+        // given
+        mockStrategyRepository.findById.mockResolvedValue(strategyFixture);
+        const randomId = TagId.generate();
+
+        const dto = {
+            actorId: ownerId.toString(),
+            strategyId: strategyId.toString(),
+            tagId: randomId.toString(),
+        };
+
+        // when & then
+        await expect(() => useCase.execute(dto)).rejects.toThrow(
+            TagNotFoundException
+        );
+        expect(mockStrategyRepository.findById).toHaveBeenCalledTimes(1);
+    });
+
+    it('태그가 삭제된다.', async () => {
+        // given
+        mockStrategyRepository.findById.mockResolvedValue(strategyFixture);
+
+        const dto = {
+            actorId: ownerId.toString(),
+            strategyId: strategyId.toString(),
+            tagId: tagId.toString(),
+        };
+
+        // when
+        await useCase.execute(dto);
+
+        // then
+        expect(mockStrategyRepository.findById).toHaveBeenCalledTimes(1);
+        expect(mockStrategyRepository.save).toHaveBeenCalledTimes(1);
+
+        const tag = strategyFixture.tags.find(tag => tag.id.equals(tagId));
+
+        expect(tag).toBeNull();
+    });
+});
