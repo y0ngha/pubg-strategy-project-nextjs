c27815873fa32064cc7541e5be7f3d0408c12c03
y0ngha <dev.y0ngha@gmail.com>
2026-01-14 17:51:50 +0900

    test: 웨이포인트 추가 테스트 케이스 작성.

---
+++ app/__tests__/application/strategy/use-cases/add-waypoint.usecase.spec.ts
@@ -0,0 +1,171 @@
+import { StrategyRepositoryPort } from '@domain/strategy/port/out/strategy-repository.port';
+import {
+    StrategyNotFoundException,
+    TeamPlayerNotFoundException,
+} from '@domain/strategy/exceptions/strategy.exceptions';
+import { Strategy } from '@domain/strategy/entities/strategy.entity';
+import { UserId } from '@domain/shared/value-objects/user-id';
+import { StrategyId } from '@domain/strategy/value-objects/strategy-id';
+import { TeamPlayerId } from '@domain/strategy/value-objects/team-player-id';
+import { PubgMap } from '@domain/strategy/enums/map.enum';
+import { Position } from '@domain/strategy/value-objects/position';
+import { AddWaypointUseCase } from '@/application/strategy/use-cases/waypoint/add-waypoint.usecase';
+
+describe('AddWaypointUseCase', () => {
+    let useCase: AddWaypointUseCase;
+    let mockStrategyRepository: jest.Mocked<StrategyRepositoryPort>;
+    let strategyFixture: Strategy;
+
+    const ownerId = UserId.generate();
+
+    let strategyId: StrategyId;
+    let teamPlayerId: TeamPlayerId;
+    const positions = [
+        {
+            x: 10,
+            y: 10,
+        },
+        {
+            x: 10,
+            y: 20,
+        },
+        {
+            x: 10,
+            y: 30,
+        },
+        {
+            x: 10,
+            y: 40,
+        },
+        {
+            x: 10,
+            y: 50,
+        },
+    ];
+
+    const title = '전략 제목';
+    const map = PubgMap.ERANGEL;
+
+    beforeEach(() => {
+        mockStrategyRepository = {
+            save: jest.fn(),
+            delete: jest.fn(),
+            findById: jest.fn(),
+            findOwnedStrategiesByUserID: jest.fn(),
+            findSharedStrategiesByUserID: jest.fn(),
+        } as jest.Mocked<StrategyRepositoryPort>;
+
+        useCase = new AddWaypointUseCase(mockStrategyRepository);
+
+        strategyFixture = Strategy.create(ownerId, title, map);
+        strategyId = strategyFixture.id;
+
+        strategyFixture.addTeamPlayer(ownerId);
+
+        teamPlayerId = strategyFixture.teamPlayers[0].id;
+    });
+
+    it('전략을 찾지 못하면, 에러를 던진다.', async () => {
+        // given
+        mockStrategyRepository.findById.mockResolvedValue(null);
+        const dto = {
+            actorId: ownerId.toString(),
+            strategyId: strategyId.toString(),
+            teamPlayerId: teamPlayerId.toString(),
+            positions: positions,
+        };
+
+        // when & then
+        await expect(() => useCase.execute(dto)).rejects.toThrow(
+            StrategyNotFoundException
+        );
+        expect(mockStrategyRepository.findById).toHaveBeenCalledTimes(1);
+    });
+
+    it('팀 플레이어를 찾지 못하면, 에러를 던진다.', async () => {
+        // given
+        mockStrategyRepository.findById.mockResolvedValue(strategyFixture);
+        const randomId = TeamPlayerId.generate();
+
+        const dto = {
+            actorId: ownerId.toString(),
+            strategyId: strategyId.toString(),
+            teamPlayerId: randomId.toString(),
+            positions: positions,
+        };
+
+        // when & then
+        await expect(() => useCase.execute(dto)).rejects.toThrow(
+            TeamPlayerNotFoundException
+        );
+        expect(mockStrategyRepository.findById).toHaveBeenCalledTimes(1);
+    });
+
+    it('웨이포인트가 없을 때 웨이포인트가 추가된다.', async () => {
+        // given
+        mockStrategyRepository.findById.mockResolvedValue(strategyFixture);
+
+        const dto = {
+            actorId: ownerId.toString(),
+            strategyId: strategyId.toString(),
+            teamPlayerId: teamPlayerId.toString(),
+            positions: positions,
+        };
+
+        // when
+        await useCase.execute(dto);
+
+        // then
+        expect(mockStrategyRepository.findById).toHaveBeenCalledTimes(1);
+        expect(mockStrategyRepository.save).toHaveBeenCalledTimes(1);
+
+        const teamPlayer = strategyFixture.teamPlayers.find(teamPlayer =>
+            teamPlayer.id.equals(teamPlayerId)
+        );
+
+        expect(teamPlayer?.waypoint).not.toBeNull();
+        expect(teamPlayer?.waypoint).not.toBeUndefined();
+        expect(teamPlayer?.waypoint?.positions).toEqual(positions);
+    });
+
+    it('웨이포인트가 있을 때 기존 웨이포인트가 교체된다.', async () => {
+        // given
+        const oldPositions = [
+            Position.create(50, 10),
+            Position.create(40, 10),
+            Position.create(30, 10),
+            Position.create(20, 10),
+            Position.create(10, 10),
+        ];
+        strategyFixture.addTeamPlayerWaypoint(
+            ownerId,
+            teamPlayerId,
+            oldPositions
+        );
+
+        mockStrategyRepository.findById.mockResolvedValue(strategyFixture);
+
+        const dto = {
+            actorId: ownerId.toString(),
+            strategyId: strategyId.toString(),
+            teamPlayerId: teamPlayerId.toString(),
+            positions: positions,
+        };
+
+        // when
+        await useCase.execute(dto);
+
+        // then
+        expect(mockStrategyRepository.findById).toHaveBeenCalledTimes(1);
+        expect(mockStrategyRepository.save).toHaveBeenCalledTimes(1);
+
+        const teamPlayer = strategyFixture.teamPlayers.find(teamPlayer =>
+            teamPlayer.id.equals(teamPlayerId)
+        );
+
+        expect(teamPlayer?.waypoint).not.toBeNull();
+        expect(teamPlayer?.waypoint).not.toBeUndefined();
+        expect(teamPlayer?.waypoint?.positions).not.toEqual(oldPositions);
+        expect(teamPlayer?.waypoint?.positions).toEqual(positions);
+    });
+});
