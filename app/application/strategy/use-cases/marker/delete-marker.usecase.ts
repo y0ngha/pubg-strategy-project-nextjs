ec0f3f16c4d3d84be24c4d894f48a256b419b13e
y0ngha <dev.y0ngha@gmail.com>
2026-01-14 17:27:01 +0900

    feat: 마커 삭제 Use Case 작성.

---
+++ app/application/strategy/use-cases/marker/delete-marker.usecase.ts
@@ -0,0 +1,30 @@
+import { inject, injectable } from 'inversify';
+import { StrategyRepositoryPort } from '@domain/strategy/port/out/strategy-repository.port';
+import {
+    DeleteMarkerRequestDto,
+    DeleteMarkerRequestSchema,
+} from '@/application/strategy/dto/marker/delete-marker.dto';
+import { StrategyNotFoundException } from '@domain/strategy/exceptions/strategy.exceptions';
+
+@injectable()
+export class DeleteMarkerUseCase {
+    constructor(
+        @inject(StrategyRepositoryPort)
+        private readonly strategyRepository: StrategyRepositoryPort
+    ) {}
+
+    async execute(dto: DeleteMarkerRequestDto): Promise<boolean> {
+        const { actorId, strategyId, teamPlayerId } =
+            DeleteMarkerRequestSchema.parse(dto);
+
+        const strategy = await this.strategyRepository.findById(strategyId);
+
+        if (!strategy) {
+            throw new StrategyNotFoundException();
+        }
+
+        strategy.removeTeamPlayerMarker(actorId, teamPlayerId);
+
+        return true;
+    }
+}
