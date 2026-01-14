2e63283ec51325e1c1e790fb058bb5dc21dcd670
y0ngha <dev.y0ngha@gmail.com>
2026-01-14 21:17:01 +0900

    feat: 자기장 삭제 Use Case 작성.

---
+++ app/application/strategy/use-cases/circle/delete-circle.usecase.ts
@@ -0,0 +1,32 @@
+import { StrategyRepositoryPort } from '@domain/strategy/port/out/strategy-repository.port';
+import { inject, injectable } from 'inversify';
+import {
+    DeleteCircleRequestDto,
+    DeleteCircleRequestSchema,
+} from '@/application/strategy/dto/circle/delete-circle.dto';
+import { StrategyNotFoundException } from '@domain/strategy/exceptions/strategy.exceptions';
+
+@injectable()
+export class DeleteCircleUseCase {
+    constructor(
+        @inject(StrategyRepositoryPort)
+        private readonly strategyRepository: StrategyRepositoryPort
+    ) {}
+
+    async execute(dto: DeleteCircleRequestDto): Promise<boolean> {
+        const { actorId, strategyId, circleId } =
+            DeleteCircleRequestSchema.parse(dto);
+
+        const strategy = await this.strategyRepository.findById(strategyId);
+
+        if (!strategy) {
+            throw new StrategyNotFoundException();
+        }
+
+        strategy.removeCircle(actorId, circleId);
+
+        await this.strategyRepository.save(strategy);
+
+        return true;
+    }
+}
