0bf139d833a2e48934f77e35c94f4706bace564e
y0ngha <dev.y0ngha@gmail.com>
2026-01-14 22:11:37 +0900

    fix: 전략 공유 권한 업데이트시, DTO 잘못 받고 있던 부분 수정. - targetUserId로 찾는게 아니라 EntityId로 찾아야함.

--- app/application/strategy/dto/share/update-strategy-share-permission.dto.ts
+++ app/application/strategy/dto/share/update-strategy-share-permission.dto.ts
@@ -2,11 +2,12 @@ import { z } from 'zod';
 import { UserId } from '@domain/shared/value-objects/user-id';
 import { StrategyId } from '@domain/strategy/value-objects/strategy-id';
 import { StrategySharePermission } from '@domain/strategy/enums/strategy-share-permission.enum';
+import { StrategyShareId } from '@domain/strategy/value-objects/strategy-share-id';
 
 export interface UpdateStrategySharePermissionRequestDto {
     actorId: string;
     strategyId: string;
-    targetUserId: string;
+    strategyShareId: string;
     permission: string;
 }
 
@@ -17,8 +18,8 @@ export const UpdateStrategySharePermissionRequestSchema = z.object({
     strategyId: z.string().transform(value => {
         return StrategyId.create(value);
     }),
-    targetUserId: z.string().transform(value => {
-        return UserId.create(value);
+    strategyShareId: z.string().transform(value => {
+        return StrategyShareId.create(value);
     }),
     permission: z.enum(StrategySharePermission),
 });
