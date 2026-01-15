2dda0eff62c03b678e780e99dd96e3ddb42a5a28
y0ngha <dev.y0ngha@gmail.com>
2026-01-15 19:25:05 +0900

    rename: get-strategy-list.dto.ts > get-strategies.dto.ts로 이름 변경.

--- app/application/strategy/dto/strategy/get-strategies.dto.ts
+++ app/application/strategy/dto/strategy/get-strategies.dto.ts
@@ -1,11 +1,11 @@
 import { z } from 'zod';
 import { UserId } from '@domain/shared/value-objects/user-id';
 
-export interface GetStrategyListRequestDto {
+export interface GetStrategiesRequestDto {
     actorId: string;
 }
 
-export const GetStrategyListRequestSchema = z.object({
+export const GetStrategiesRequestSchema = z.object({
     actorId: z.string().transform(value => {
         return UserId.create(value);
     }),
