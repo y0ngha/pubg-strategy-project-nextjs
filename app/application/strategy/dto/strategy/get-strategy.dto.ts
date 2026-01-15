77cd80afb6c3c34231ac1461d88ee4a01dc1f5d3
y0ngha <dev.y0ngha@gmail.com>
2026-01-15 19:45:34 +0900

    feat: 전략 가져오기 DTO 작성. - 상세 전략과 전략 리스트는 객체를 공유하므로 파일 통합.

--- app/application/strategy/dto/strategy/get-strategy.dto.ts
+++ app/application/strategy/dto/strategy/get-strategy.dto.ts
@@ -15,3 +15,101 @@ export const GetStrategyRequestSchema = z.object({
         return StrategyId.create(value);
     }),
 });
+
+export interface GetStrategiesRequestDto {
+    actorId: string;
+}
+
+export const GetStrategiesRequestSchema = z.object({
+    actorId: z.string().transform(value => {
+        return UserId.create(value);
+    }),
+});
+
+interface Position {
+    x: number;
+    y: number;
+}
+
+interface WaypointResponseDto {
+    id: string;
+    positions: Position[];
+}
+
+interface MarkerResponseDto {
+    id: string;
+    position: Position;
+}
+
+interface TeamPlayerResponseDto {
+    id: string;
+    priority: number;
+    position: Position;
+    color: string;
+    marker?: MarkerResponseDto;
+    waypoint?: WaypointResponseDto;
+}
+
+interface EnemyTeamResponseDto {
+    id: string;
+    teamLabel: string;
+    position: Position;
+}
+
+interface CircleResponseDto {
+    id: string;
+    centerPosition: Position;
+    phase: number;
+    radius: number;
+    color: string;
+}
+
+interface AirplanePathResponseDto {
+    id: string;
+    startPosition: Position;
+    endPosition: Position;
+}
+
+interface TagResponseDto {
+    id: string;
+    position: Position;
+    content: string;
+}
+
+interface StrategyShareResponseDto {
+    id: string;
+    sharedUserId: string;
+    sharedEmail: string;
+    permission: string;
+}
+
+interface CommentResponseDto {
+    id: string;
+    authorId: string;
+    authorEmail: string;
+    position: Position;
+    content: string;
+    childComments: Omit<CommentResponseDto, 'position'>;
+}
+
+export interface GetStrategyResponseDto {
+    id: string;
+    ownerId: string;
+    title: string;
+    map: string;
+
+    teamPlayers: TeamPlayerResponseDto[];
+    enemyTeams: EnemyTeamResponseDto[];
+    circles: CircleResponseDto[];
+    airplanePath?: AirplanePathResponseDto;
+    tags: TagResponseDto[];
+    shares: StrategyShareResponseDto[];
+    comments: CommentResponseDto[];
+
+    permission: string;
+
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export type GetStrategiesResponseDto = GetStrategyResponseDto[];
