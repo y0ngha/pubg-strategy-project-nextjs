ca64bffd83aca8eb2ace6ccf0fda2a67c273882f
y0ngha <dev.y0ngha@gmail.com>
2026-01-15 17:56:35 +0900

    fix: content string으로 선언되어 있는 것 VO로 변경.

--- app/application/strategy/dto/comment/create-comment.dto.ts
+++ app/application/strategy/dto/comment/create-comment.dto.ts
@@ -4,6 +4,7 @@ import { StrategyId } from '@domain/strategy/value-objects/strategy-id';
 import { Position } from '@domain/strategy/value-objects/position';
 import { CommentId } from '@domain/strategy/value-objects/comment-id';
 import { Email } from '@domain/shared/value-objects/email';
+import { CommentContent } from '@domain/strategy/value-objects/comment-content';
 
 export interface CreateCommentRequestDto {
     actorId: string;
@@ -24,7 +25,9 @@ export const CreateCommentRequestSchema = z.object({
     strategyId: z.string().transform(value => {
         return StrategyId.create(value);
     }),
-    content: z.string(),
+    content: z.string().transform(value => {
+        return CommentContent.create(value);
+    }),
     parentCommentId: z
         .string()
         .transform(value => {
