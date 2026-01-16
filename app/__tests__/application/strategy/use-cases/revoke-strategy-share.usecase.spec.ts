ef5ed1606593d18a403d7616e546fbb36728353d
y0ngha <dev.y0ngha@gmail.com>
2026-01-16 20:58:21 +0900

    test: 전략 공유 권한 회수 Use Case 테스트 케이스에 도메인 엔티티 예외 전파 추가.

--- app/__tests__/application/strategy/use-cases/revoke-strategy-share.usecase.spec.ts
+++ app/__tests__/application/strategy/use-cases/revoke-strategy-share.usecase.spec.ts
@@ -1,6 +1,7 @@
 import { StrategyRepositoryPort } from '@domain/strategy/port/out/strategy-repository.port';
 import {
     StrategyNotFoundException,
+    StrategyPermissionDeniedException,
     StrategyShareNotFoundException,
 } from '@domain/strategy/exceptions/strategy.exceptions';
 import { Strategy } from '@domain/strategy/entities/strategy.entity';
@@ -103,6 +104,29 @@ describe('RevokeStrategyShareUseCase', () => {
 
         expect(strategyShare?.permission).toEqual(
             StrategySharePermission.ACCESS_DENIED
+        );
+    });
+
+    it('도메인 엔티티에서 에러 발생시 에러를 전파한다', async () => {
+        // Given
+        jest.spyOn(
+            strategyFixture,
+            'updateStrategySharePermission'
+        ).mockImplementation(() => {
+            throw new StrategyPermissionDeniedException();
+        });
+
+        mockStrategyRepository.findById.mockResolvedValue(strategyFixture);
+
+        const dto = {
+            actorId: ownerId.toString(),
+            strategyId: strategyId.toString(),
+            strategyShareId: strategyShareId.toString(),
+        };
+
+        // When & Then
+        await expect(useCase.execute(dto)).rejects.toThrow(
+            StrategyPermissionDeniedException
         );
     });
 });
