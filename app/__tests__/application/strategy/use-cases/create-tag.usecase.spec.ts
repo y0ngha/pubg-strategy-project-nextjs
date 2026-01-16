94686edf7899fb8eb04b3c8edcf82750d3a8753e
y0ngha <dev.y0ngha@gmail.com>
2026-01-16 21:03:00 +0900

    test: 태그 생성 Use Case 테스트 케이스에 도메인 엔티티 예외 전파 추가.

--- app/__tests__/application/strategy/use-cases/create-tag.usecase.spec.ts
+++ app/__tests__/application/strategy/use-cases/create-tag.usecase.spec.ts
@@ -69,4 +69,18 @@ describe('CreateTagUseCase', () => {
         expect(strategyFixture.tags).toHaveLength(1);
         expect(tag?.content.toString()).toEqual(dto.content);
     });
+
+    it('에러 발생시 에러를 전파한다', async () => {
+        // Given
+        mockStrategyRepository.findById.mockRejectedValue(new Error());
+
+        const dto = {
+            actorId: ownerId.toString(),
+            strategyId: strategyId.toString(),
+            content: '내용입니다.',
+        };
+
+        // When & Then
+        await expect(useCase.execute(dto)).rejects.toThrow(Error);
+    });
 });
