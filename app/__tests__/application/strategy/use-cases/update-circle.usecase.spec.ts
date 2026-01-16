71c3c79dffa23851774da27d80325a04fcecfa5f
y0ngha <dev.y0ngha@gmail.com>
2026-01-16 20:57:03 +0900

    test: 자기장 수정 Use Case 테스트 케이스에 도메인 엔티티 예외 전파 추가.

--- app/__tests__/application/strategy/use-cases/update-circle.usecase.spec.ts
+++ app/__tests__/application/strategy/use-cases/update-circle.usecase.spec.ts
@@ -1,6 +1,7 @@
 import { StrategyRepositoryPort } from '@domain/strategy/port/out/strategy-repository.port';
 import {
     CircleNotFoundException,
+    StrategyEditPermissionDeniedException,
     StrategyNotFoundException,
 } from '@domain/strategy/exceptions/strategy.exceptions';
 import { Strategy } from '@domain/strategy/entities/strategy.entity';
@@ -189,5 +190,29 @@ describe('UpdateCircleUseCase', () => {
         // then
         expect(mockStrategyRepository.findById).toHaveBeenCalledTimes(0);
         expect(mockStrategyRepository.save).toHaveBeenCalledTimes(0);
+    });
+
+    it('도메인 엔티티에서 에러 발생시 에러를 전파한다', async () => {
+        // Given
+        jest.spyOn(strategyFixture, 'updateCircle').mockImplementation(() => {
+            throw new StrategyEditPermissionDeniedException();
+        });
+
+        mockStrategyRepository.findById.mockResolvedValue(strategyFixture);
+
+        const dto = {
+            actorId: ownerId.toString(),
+            strategyId: strategyId.toString(),
+            circleId: circleId.toString(),
+            centerPosition: {
+                x: 10,
+                y: 200,
+            },
+        };
+
+        // When & Then
+        await expect(useCase.execute(dto)).rejects.toThrow(
+            StrategyEditPermissionDeniedException
+        );
     });
 });
