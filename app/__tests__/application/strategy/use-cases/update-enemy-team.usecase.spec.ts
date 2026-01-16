05e2766f82ada62e2026c9c28e005540734161da
y0ngha <dev.y0ngha@gmail.com>
2026-01-16 20:56:27 +0900

    test: 적 팀 수정 Use Case 테스트 케이스에 도메인 엔티티 예외 전파 추가.

--- app/__tests__/application/strategy/use-cases/update-enemy-team.usecase.spec.ts
+++ app/__tests__/application/strategy/use-cases/update-enemy-team.usecase.spec.ts
@@ -1,6 +1,7 @@
 import { StrategyRepositoryPort } from '@domain/strategy/port/out/strategy-repository.port';
 import {
     EnemyTeamNotFoundException,
+    StrategyEditPermissionDeniedException,
     StrategyNotFoundException,
 } from '@domain/strategy/exceptions/strategy.exceptions';
 import { Strategy } from '@domain/strategy/entities/strategy.entity';
@@ -190,5 +191,31 @@ describe('UpdateEnemyTeamUseCase', () => {
         // then
         expect(mockStrategyRepository.findById).toHaveBeenCalledTimes(0);
         expect(mockStrategyRepository.save).toHaveBeenCalledTimes(0);
+    });
+
+    it('도메인 엔티티에서 에러 발생시 에러를 전파한다', async () => {
+        // Given
+        jest.spyOn(strategyFixture, 'updateEnemyTeam').mockImplementation(
+            () => {
+                throw new StrategyEditPermissionDeniedException();
+            }
+        );
+
+        mockStrategyRepository.findById.mockResolvedValue(strategyFixture);
+
+        const dto = {
+            actorId: ownerId.toString(),
+            strategyId: strategyId.toString(),
+            enemyTeamId: enemyTeamId.toString(),
+            position: {
+                x: 10,
+                y: 200,
+            },
+        };
+
+        // When & Then
+        await expect(useCase.execute(dto)).rejects.toThrow(
+            StrategyEditPermissionDeniedException
+        );
     });
 });
