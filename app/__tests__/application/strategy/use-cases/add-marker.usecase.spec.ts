aabec7cb2da8ac460744d999ffc974a3fb29cb07
y0ngha <dev.y0ngha@gmail.com>
2026-01-16 21:08:09 +0900

    test: 마커 생성 Use Case 테스트 케이스에 도메인 엔티티 예외 전파 추가.

--- app/__tests__/application/strategy/use-cases/add-marker.usecase.spec.ts
+++ app/__tests__/application/strategy/use-cases/add-marker.usecase.spec.ts
@@ -1,6 +1,7 @@
 import { AddMarkerUseCase } from '@/application/strategy/use-cases/marker/add-marker.usecase';
 import { StrategyRepositoryPort } from '@domain/strategy/port/out/strategy-repository.port';
 import {
+    StrategyEditPermissionDeniedException,
     StrategyNotFoundException,
     TeamPlayerNotFoundException,
 } from '@domain/strategy/exceptions/strategy.exceptions';
@@ -134,5 +135,28 @@ describe('AddMarkerUseCase', () => {
         expect(teamPlayer?.marker).not.toBeUndefined();
         expect(teamPlayer?.marker?.position).not.toEqual(oldPosition);
         expect(teamPlayer?.marker?.position).toEqual(position);
+    });
+
+    it('에러 발생시 에러를 전파한다', async () => {
+        // Given
+        jest.spyOn(strategyFixture, 'addTeamPlayerMarker').mockImplementation(
+            () => {
+                throw new StrategyEditPermissionDeniedException();
+            }
+        );
+
+        mockStrategyRepository.findById.mockResolvedValue(strategyFixture);
+
+        const dto = {
+            actorId: ownerId.toString(),
+            strategyId: strategyId.toString(),
+            teamPlayerId: teamPlayerId.toString(),
+            position: position,
+        };
+
+        // When & Then
+        await expect(useCase.execute(dto)).rejects.toThrow(
+            StrategyEditPermissionDeniedException
+        );
     });
 });
