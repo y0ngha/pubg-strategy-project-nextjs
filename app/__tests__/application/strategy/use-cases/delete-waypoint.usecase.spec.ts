1f76d1524470139f9da196fbb9195b4e24dbede7
y0ngha <dev.y0ngha@gmail.com>
2026-01-16 20:59:12 +0900

    test: 웨이포인트 삭제 Use Case 테스트 케이스에 도메인 엔티티 예외 전파 추가.

--- app/__tests__/application/strategy/use-cases/delete-waypoint.usecase.spec.ts
+++ app/__tests__/application/strategy/use-cases/delete-waypoint.usecase.spec.ts
@@ -1,5 +1,6 @@
 import { StrategyRepositoryPort } from '@domain/strategy/port/out/strategy-repository.port';
 import {
+    StrategyEditPermissionDeniedException,
     StrategyNotFoundException,
     TeamPlayerNotFoundException,
 } from '@domain/strategy/exceptions/strategy.exceptions';
@@ -103,5 +104,28 @@ describe('DeleteWaypointUseCase', () => {
         );
 
         expect(teamPlayer?.waypoint).toBeNull();
+    });
+
+    it('도메인 엔티티에서 에러 발생시 에러를 전파한다', async () => {
+        // Given
+        jest.spyOn(
+            strategyFixture,
+            'removeTeamPlayerWaypoint'
+        ).mockImplementation(() => {
+            throw new StrategyEditPermissionDeniedException();
+        });
+
+        mockStrategyRepository.findById.mockResolvedValue(strategyFixture);
+
+        const dto = {
+            actorId: ownerId.toString(),
+            strategyId: strategyId.toString(),
+            teamPlayerId: teamPlayerId.toString(),
+        };
+
+        // When & Then
+        await expect(useCase.execute(dto)).rejects.toThrow(
+            StrategyEditPermissionDeniedException
+        );
     });
 });
