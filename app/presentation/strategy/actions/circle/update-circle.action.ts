133de55b901d699f1ca19d2e9125b25a888d548b
y0ngha <dev.y0ngha@gmail.com>
2026-01-19 19:16:36 +0900

    feat: 자기장 수정 Server Action 작성.

---
+++ app/presentation/strategy/actions/circle/update-circle.action.ts
@@ -0,0 +1,56 @@
+'use server';
+
+import { initializeRequestServices } from '@global/di/server/get-server-dependency';
+import { UpdateCircleUseCase } from '@/application/strategy/use-cases/circle/update-circle.usecase';
+import { getRequiredFormData } from '@/presentation/helpers/form-data.helper';
+
+export async function updateCircleAction(_: unknown, formData: FormData) {
+    const getService = initializeRequestServices();
+
+    const {
+        userId,
+        strategyId,
+        circleId,
+        phase,
+        centerPositionX,
+        centerPositionY,
+    } = getRequiredFormData(formData, [
+        {
+            key: 'userId',
+            error: '유저 고유 식별자를 불러올 수 없습니다.',
+            type: 'string',
+        },
+        {
+            key: 'strategyId',
+            error: '전략 고유 식별자를 불러올 수 없습니다.',
+            type: 'string',
+        },
+        {
+            key: 'circleId',
+            error: '자기장 고유 식별자를 불러올 수 없습니다.',
+            type: 'string',
+        },
+        { key: 'phase', type: 'number', allowUndefined: true },
+        { key: 'centerPositionX', type: 'number', allowUndefined: true },
+        { key: 'centerPositionY', type: 'number', allowUndefined: true },
+    ] as const);
+
+    const useCase = getService(UpdateCircleUseCase);
+
+    const hasPosition = centerPositionX != null && centerPositionY != null;
+
+    const dto = {
+        actorId: userId,
+        strategyId: strategyId,
+        circleId: circleId,
+        phase: phase,
+        ...(hasPosition && {
+            centerPosition: {
+                x: centerPositionX,
+                y: centerPositionY,
+            },
+        }),
+    };
+
+    return await useCase.execute(dto);
+}
