5a66e8f7b401e4b10fddfb2cf1d1a455b69c24ce
y0ngha <dev.y0ngha@gmail.com>
2026-01-19 21:11:19 +0900

    fix: 마커 수정 Server Action에서 markerId를 갖고와서 수정하고 있던 부분 수정. - 마커 수정시에는 TeamPlayerId를 가지고 수정하면 됨.

--- app/presentation/strategy/actions/marker/update-marker.action.ts
+++ app/presentation/strategy/actions/marker/update-marker.action.ts
@@ -7,35 +7,38 @@ import { UpdateMarkerUseCase } from '@/application/strategy/use-cases/marker/upd
 export async function updateMarkerAction(_: unknown, formData: FormData) {
     const getService = initializeRequestServices();
 
-    const { userId, strategyId, markerId, position } = parseFormData(formData, [
-        {
-            key: 'userId',
-            error: '유저 고유 식별자를 불러올 수 없습니다.',
-            type: 'string',
-        },
-        {
-            key: 'strategyId',
-            error: '전략 고유 식별자를 불러올 수 없습니다.',
-            type: 'string',
-        },
-        {
-            key: 'markerId',
-            error: '마커 고유 식별자를 불러올 수 없습니다.',
-            type: 'string',
-        },
-        {
-            key: 'position',
-            error: '마커 위치를 불러올 수 없습니다.',
-            type: 'position',
-        },
-    ] as const);
+    const { userId, strategyId, teamPlayerId, position } = parseFormData(
+        formData,
+        [
+            {
+                key: 'userId',
+                error: '유저 고유 식별자를 불러올 수 없습니다.',
+                type: 'string',
+            },
+            {
+                key: 'strategyId',
+                error: '전략 고유 식별자를 불러올 수 없습니다.',
+                type: 'string',
+            },
+            {
+                key: 'teamPlayerId',
+                error: '팀 플레이어 고유 식별자를 불러올 수 없습니다.',
+                type: 'string',
+            },
+            {
+                key: 'position',
+                error: '마커 위치를 불러올 수 없습니다.',
+                type: 'position',
+            },
+        ] as const
+    );
 
     const useCase = getService(UpdateMarkerUseCase);
 
     const dto = {
         actorId: userId,
         strategyId: strategyId,
-        teamPlayerId: markerId,
+        teamPlayerId: teamPlayerId,
         position: position,
     };
 
