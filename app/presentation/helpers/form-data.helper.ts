30a5d69f1026b5bdaddbe4dd86c12d2808ca27b5
y0ngha <dev.y0ngha@gmail.com>
2026-01-19 20:41:51 +0900

    feat: form-data 파싱 진행시 array도 처리될 수 있도록 기능 추가.

--- app/presentation/helpers/form-data.helper.ts
+++ app/presentation/helpers/form-data.helper.ts
@@ -11,20 +11,23 @@ interface Field {
     error?: string;
     type: keyof FieldTypeMap;
     allowUndefined?: boolean;
+    isArray?: boolean;
 }
 
 type Parsers = {
     [K in keyof FieldTypeMap]: (
-        value: FormDataEntryValue,
+        value: unknown,
         error?: string
     ) => FieldTypeMap[K];
 };
 
 type ParsedFormData<T extends readonly Field[]> = {
     [K in T[number] as K['key']]: K['allowUndefined'] extends true
-        ? FieldTypeMap[K['type']] | undefined
-        : FieldTypeMap[K['type']];
+        ? BaseType<FieldTypeMap[K['type']], K['isArray']> | undefined
+        : BaseType<FieldTypeMap[K['type']], K['isArray']>;
 };
+
+type BaseType<T, IsArray> = IsArray extends true ? T[] : T;
 
 export function parseFormData<T extends readonly Field[]>(
     formData: FormData,
@@ -35,9 +38,12 @@ export function parseFormData<T extends readonly Field[]>(
     formData: FormData,
     fields: T
 ) {
-    const data: Record<string, string | number | Position> = {};
+    const data: Record<
+        string,
+        FieldTypeMap[keyof FieldTypeMap] | FieldTypeMap[keyof FieldTypeMap][]
+    > = {};
 
-    for (const { key, error, type, allowUndefined } of fields) {
+    for (const { key, error, type, allowUndefined, isArray } of fields) {
         const errorMessage = error ? error : `${key}를 불러올 수 없습니다.`;
 
         const value = formData.get(key);
@@ -48,21 +54,39 @@ export function parseFormData<T extends readonly Field[]>(
             throw new Error(errorMessage);
         }
 
-        data[key] = parsers[type](value, errorMessage);
+        if (isArray) {
+            try {
+                const parsedArray = JSON.parse(value.toString());
+
+                if (!Array.isArray(parsedArray)) {
+                    throw new Error(errorMessage);
+                }
+
+                data[key] = parsedArray.map(item =>
+                    parsers[type](item, errorMessage)
+                );
+            } catch {
+                throw new Error(errorMessage);
+            }
+        } else {
+            data[key] = parsers[type](value, errorMessage);
+        }
     }
 
     return data;
 }
 
 const parsers: Parsers = {
-    string: (value: FormDataEntryValue) => value.toString(),
+    string: (value: unknown) => String(value),
 
-    number: (value: FormDataEntryValue, error?: string) =>
-        safeParseNumber(value, error),
+    number: (value: unknown, error?: string) => safeParseNumber(value, error),
 
-    position: (value: FormDataEntryValue, error?: string) => {
+    position: (value: unknown, error?: string) => {
         try {
-            const json = JSON.parse(value.toString());
+            const json =
+                typeof value === 'string'
+                    ? JSON.parse(value.toString())
+                    : value;
 
             const x = safeParseNumber(json?.x, error);
             const y = safeParseNumber(json?.y, error);
