be32342cd03f85b3dddaea26f76ebc12b0eeece9
y0ngha <dev.y0ngha@gmail.com>
2026-01-19 18:06:54 +0900

    feat: 비행기 동선 삭제 Server Action 작성.

---
+++ app/presentation/strategy/actions/airplane-path/delete-airplane-path.action.ts
@@ -0,0 +1,25 @@
+'use server';
+
+import { initializeRequestServices } from '@global/di/server/get-server-dependency';
+import { DeleteAirplanePathUseCase } from '@/application/strategy/use-cases/airplane-path/delete-airplane-path.usecase';
+import { getRequiredFormData } from '@/presentation/helpers/form-data.helper';
+
+export async function deleteAirplanePathAction(_: unknown, formData: FormData) {
+    const getService = initializeRequestServices();
+
+    const { userId, strategyId } = getRequiredFormData(formData, [
+        { key: 'userId', error: '유저 고유 식별자를 불러올 수 없습니다.' },
+        { key: 'strategyId', error: '전략 고유 식별자를 불러올 수 없습니다.' },
+    ]);
+
+    const useCase = getService<DeleteAirplanePathUseCase>(
+        DeleteAirplanePathUseCase
+    );
+
+    const dto = {
+        actorId: userId,
+        strategyId: strategyId,
+    };
+
+    return await useCase.execute(dto);
+}
