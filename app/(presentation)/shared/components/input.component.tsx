e6e53727a80c23bd36785f287c2b814f4fee4694
y0ngha <dev.y0ngha@gmail.com>
2026-01-20 21:34:51 +0900

    feat: label, input id 공유 추가

--- app/(presentation)/shared/components/input.component.tsx
+++ app/(presentation)/shared/components/input.component.tsx
@@ -1,5 +1,6 @@
 import { cva, VariantProps } from 'class-variance-authority';
 import { cn } from '@/(presentation)/shared/utils/class-names.util';
+import { useId } from 'react';
 
 const InputVariants = cva(
     'flex h-11 w-full rounded-lg border border-border bg-surface px-3 py-2 text-sm text-foreground shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-zinc-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary disabled:cursor-not-allowed disabled:opacity-50',
@@ -36,10 +37,13 @@ function Input({
     disabled,
     ...props
 }: InputProps) {
+    const id = useId();
+
     return (
         <div className="w-full space-y-1">
             {label && (
                 <label
+                    htmlFor={id}
                     className={cn(
                         'text-muted-foreground text-sm leading-none font-medium peer-disabled:cursor-not-allowed peer-disabled:opacity-70',
                         disabled && 'cursor-not-allowed opacity-70'
@@ -49,6 +53,7 @@ function Input({
                 </label>
             )}
             <input
+                id={id}
                 disabled={disabled}
                 type={type}
                 className={cn(
