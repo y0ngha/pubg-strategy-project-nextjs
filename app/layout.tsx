f90b9f818d392d33afb8357f66d3fe3fb08ae159
y0ngha <dev.y0ngha@gmail.com>
2026-01-22 04:16:18 +0900

    feat: layout에 ToastContainer 추가.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
 const geistSans = Geist({
     variable: '--font-geist-sans',
@@ -28,6 +30,7 @@ export default function RootLayout({
                 className={`${geistSans.variable} ${geistMono.variable} h-screen antialiased`}
             >
                 {children}
+                <ToastContainer position={'bottom-center'} theme={'dark'} />
             </body>
         </html>
     );
