211266acb957a451f03d51a7ae6a0d959d118cbf
y0ngha <dev.y0ngha@gmail.com>
2026-01-21 18:49:01 +0900

    feat: Tabs Component 작성.

---
+++ app/(presentation)/shared/components/tabs.component.tsx
@@ -0,0 +1,96 @@
+'use client';
+
+import { cva, type VariantProps } from 'class-variance-authority';
+import { cn } from '@/(presentation)/shared/utils/class-names.util';
+import React from 'react';
+
+type TabsContextValue = {
+    value: string;
+    onValueChange: (value: string) => void;
+};
+
+const TabsContext = React.createContext<TabsContextValue | undefined>(
+    undefined
+);
+
+interface TabsProps extends React.HTMLAttributes<HTMLDivElement> {
+    value: string;
+    onValueChange: (value: string) => void;
+}
+
+function Tabs({
+    value,
+    onValueChange,
+    children,
+    className,
+    ...props
+}: TabsProps) {
+    return (
+        <TabsContext.Provider value={{ value, onValueChange }}>
+            <div className={cn('w-full', className)} {...props}>
+                {children}
+            </div>
+        </TabsContext.Provider>
+    );
+}
+
+function List({
+    className,
+    children,
+    ...props
+}: React.HTMLAttributes<HTMLDivElement>) {
+    return (
+        <div
+            className={cn('border-border flex space-x-6 border-b', className)}
+            {...props}
+        >
+            {children}
+        </div>
+    );
+}
+
+const TabsItemVariants = cva(
+    'whitespace-nowrap border-b-2 px-1 py-4 text-sm font-medium transition-colors cursor-pointer focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary disabled:pointer-events-none disabled:opacity-50',
+    {
+        variants: {
+            active: {
+                true: 'border-primary text-primary',
+                false: 'border-transparent text-muted hover:border-zinc-700 hover:text-zinc-300',
+            },
+        },
+        defaultVariants: {
+            active: false,
+        },
+    }
+);
+
+interface TabsItemProps
+    extends
+        React.ButtonHTMLAttributes<HTMLButtonElement>,
+        VariantProps<typeof TabsItemVariants> {
+    value: string;
+}
+
+function Item({ className, value, children, ...props }: TabsItemProps) {
+    const context = React.useContext(TabsContext);
+
+    const isActive = context?.value === value;
+
+    return (
+        <button
+            type="button"
+            className={cn(TabsItemVariants({ active: isActive }), className)}
+            onClick={() => context?.onValueChange(value)}
+            {...props}
+        >
+            {children}
+        </button>
+    );
+}
+
+Tabs.List = List;
+Tabs.Item = Item;
+
+Tabs.displayName = 'Tabs';
+
+export { Tabs };
