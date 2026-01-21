b87d38baf6a00c2857af6151a20bae0a85269072
y0ngha <dev.y0ngha@gmail.com>
2026-01-21 22:38:13 +0900

    chore: eslint에서 react-compiler 활성화.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,20 @@
 import { defineConfig, globalIgnores } from 'eslint/config';
 import nextVitals from 'eslint-config-next/core-web-vitals';
 import nextTs from 'eslint-config-next/typescript';
+import reactCompiler from 'eslint-plugin-react-compiler';
 
 const eslintConfig = defineConfig([
     ...nextVitals,
     ...nextTs,
+    {
+        plugins: {
+            'react-compiler': reactCompiler,
+        },
+        rules: {
+            'react-compiler/react-compiler': 'error',
+            'react-hooks/exhaustive-deps': 'off',
+        },
+    },
     // Override default ignores of eslint-config-next.
     globalIgnores([
         // Default ignores of eslint-config-next:
@@ -12,6 +22,7 @@ const eslintConfig = defineConfig([
         'out/**',
         'build/**',
         'next-env.d.ts',
+        'coverage/**',
     ]),
 ]);
 
