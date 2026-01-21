95b2b70df245b9c22631fb91e3becb4268d60a06
y0ngha <dev.y0ngha@gmail.com>
2026-01-21 19:25:44 +0900

    design: Skeleton Component 색깔 변경

--- app/(presentation)/shared/components/skeleton.component.tsx
+++ app/(presentation)/shared/components/skeleton.component.tsx
@@ -3,7 +3,7 @@ type SkeletonProps = React.HTMLAttributes<HTMLDivElement>;
 function Skeleton({ className, ...props }: SkeletonProps) {
     return (
         <div
-            className={`w-full animate-pulse rounded-md bg-zinc-300/50 ${className}`}
+            className={`w-full animate-pulse rounded-md bg-zinc-800/50 ${className}`}
             {...props}
         />
     );
