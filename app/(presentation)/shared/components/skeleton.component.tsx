2149f6c522aea4110f59f7bcba8d111b21619560
y0ngha <dev.y0ngha@gmail.com>
2026-01-21 19:25:15 +0900

    feat: Sekeleton Component 작성.

---
+++ app/(presentation)/shared/components/skeleton.component.tsx
@@ -0,0 +1,14 @@
+type SkeletonProps = React.HTMLAttributes<HTMLDivElement>;
+
+function Skeleton({ className, ...props }: SkeletonProps) {
+    return (
+        <div
+            className={`w-full animate-pulse rounded-md bg-zinc-300/50 ${className}`}
+            {...props}
+        />
+    );
+}
+
+Skeleton.displayName = 'Skeleton';
+
+export default Skeleton;
