ff379479cd0574ae5a2e5308537a077cd84b37ea
y0ngha <dev.y0ngha@gmail.com>
2026-01-22 20:55:52 +0900

    refactor: nbsp > gap-2로 변경

--- app/(presentation)/(pages)/login/components/social-login.component.tsx
+++ app/(presentation)/(pages)/login/components/social-login.component.tsx
@@ -6,9 +6,9 @@ import Google from '@/(presentation)/shared/icons/google.icon';
 function SocialLogin() {
     return (
         <div className={'flex w-full flex-row gap-4'}>
-            <Button variant={'outline'} className={'flex-1'}>
+            <Button variant={'outline'} className={'flex-1 gap-2'}>
                 <Google width={18} height={18} />
-                &nbsp;구글로 계속하기
+                구글로 계속하기
             </Button>
         </div>
     );
