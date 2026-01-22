29a91d0eb92acd8724dee694a7f98f11ab066a7b
y0ngha <dev.y0ngha@gmail.com>
2026-01-22 22:01:59 +0900

    feat: 회원가입 커스텀 훅 작성.

---
+++ app/(presentation)/(pages)/register/hooks/useEmailRegister.ts
@@ -0,0 +1,41 @@
+import { useActionState, useEffect } from 'react';
+import {
+    RegisterWithEmailAction,
+    registerWithEmailAction,
+} from '@/(presentation)/user/actions/register-with-email.action';
+import { toast } from 'react-toastify';
+import { redirect } from 'next/navigation';
+
+export function useEmailRegister() {
+    const [state, formAction, isPending] = useActionState<
+        RegisterWithEmailAction,
+        FormData
+    >(registerWithEmailAction, {
+        isSuccess: false,
+        isError: false,
+        errorMessage: undefined,
+        data: undefined,
+    });
+
+    const { isSuccess, isError, errorMessage } = state;
+
+    useEffect(() => {
+        if (isError) {
+            toast.error(
+                errorMessage ?? '알 수 없는 오류로 로그인에 실패했습니다.'
+            );
+        }
+    }, [isError, errorMessage]);
+
+    useEffect(() => {
+        if (isSuccess) {
+            toast.success('회원가입이 완료되었습니다.');
+            redirect('/login');
+        }
+    }, [isSuccess]);
+
+    return {
+        formAction,
+        isPending,
+    };
+}
