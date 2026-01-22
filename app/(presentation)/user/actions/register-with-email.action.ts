b8ca5175de7a0a48e618f8e4ccebe629a07cad99
y0ngha <dev.y0ngha@gmail.com>
2026-01-22 21:35:40 +0900

    feat: 이메일 회원가입 시점에서 패스워드 재검증 할 수 있도록 수정.

--- app/(presentation)/user/actions/register-with-email.action.ts
+++ app/(presentation)/user/actions/register-with-email.action.ts
@@ -3,11 +3,17 @@
 import { initializeRequestServices } from '@global/di/server/get-server-dependency';
 import { RegisterWithEmailUseCase } from '@/application/user/use-cases/register-with-email.usecase';
 import { parseFormData } from '@/(presentation)/shared/helpers/form-data.helper';
+import { ServerAction } from '@/(presentation)/shared/types/server-action';
 
-export async function registerWithEmailAction(_: unknown, formData: FormData) {
+type RegisterWithEmailAction = ServerAction;
+
+export async function registerWithEmailAction(
+    _: unknown,
+    formData: FormData
+): Promise<RegisterWithEmailAction> {
     const getService = initializeRequestServices();
 
-    const { email, password } = parseFormData(formData, [
+    const { email, password, confirmPassword } = parseFormData(formData, [
         {
             key: 'email',
             error: '이메일은 필수적으로 입력해야합니다.',
@@ -18,7 +24,21 @@ export async function registerWithEmailAction(_: unknown, formData: FormData) {
             error: '비밀번호는 필수적으로 입력해야합니다.',
             type: 'string',
         },
+        {
+            key: 'confirmPassword',
+            error: '비밀번호는 필수적으로 입력해야합니다.',
+            type: 'string',
+        },
     ] as const);
+
+    if (password !== confirmPassword) {
+        return {
+            isSuccess: false,
+            isError: true,
+            errorMessage: '비밀번호가 일치하지 않습니다.',
+            data: undefined,
+        };
+    }
 
     const dto = {
         email: email,
