9bc74fcf918fe8ecc133627e0c957265d8f02b0c
y0ngha <dev.y0ngha@gmail.com>
2026-01-22 17:07:23 +0900

    feat: 인증 서비스에서 로그인 진행시 token 반환하도록 수정.

--- app/domain/user/port/out/authentication-service.port.ts
+++ app/domain/user/port/out/authentication-service.port.ts
@@ -3,9 +3,15 @@ import { Password } from '../../value-objects/password';
 import { UserId } from '@/domain/shared/value-objects/user-id';
 
 export abstract class AuthenticationServicePort {
-    abstract login(email: Email, password: Password): Promise<boolean>;
+    abstract login(
+        email: Email,
+        password: Password
+    ): Promise<{ accessToken: string; refreshToken: string }>;
 
-    abstract googleLogin(email: Email, token: string): Promise<boolean>;
+    abstract googleLogin(
+        email: Email,
+        token: string
+    ): Promise<{ accessToken: string; refreshToken: string }>;
 
     abstract logout(userId: UserId): Promise<boolean>;
 }
