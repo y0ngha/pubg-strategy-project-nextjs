14cfa1d1820165b98f147977e493e41c826e100d
y0ngha <dev.y0ngha@gmail.com>
2026-01-22 17:06:28 +0900

    feat: 로그인 컴포넌트에서 로그인 진행중일 때 disabled 처리 진행.

--- app/(presentation)/login/components/email-login.component.tsx
+++ app/(presentation)/login/components/email-login.component.tsx
@@ -5,13 +5,23 @@ import Button from '@/(presentation)/shared/components/button.component';
 import { useEmailLogin } from '@/(presentation)/login/hooks/useEmailLogin';
 
 function EmailLogin() {
-    const { formAction } = useEmailLogin();
+    const { formAction, isPending } = useEmailLogin();
 
     return (
         <form className="w-full space-y-4" action={formAction}>
-            <Input name={'email'} type={'text'} label={'Email'} />
-            <Input name={'password'} type={'password'} label={'Password'} />
-            <Button type={'submit'} className={'w-full'}>
+            <Input
+                name={'email'}
+                type={'text'}
+                label={'Email'}
+                disabled={isPending}
+            />
+            <Input
+                name={'password'}
+                type={'password'}
+                label={'Password'}
+                disabled={isPending}
+            />
+            <Button type={'submit'} className={'w-full'} disabled={isPending}>
                 로그인
             </Button>
         </form>
