a2129370e109437c7e59301700ae9e46523cd3a0
y0ngha <dev.y0ngha@gmail.com>
2026-01-22 22:02:44 +0900

    feat: 회원가입 화면에 이용약관 동의 체크박스 추가.

--- app/(presentation)/(pages)/register/components/email-register.component.tsx
+++ app/(presentation)/(pages)/register/components/email-register.component.tsx
@@ -3,6 +3,9 @@
 import Input from '@/(presentation)/shared/components/input.component';
 import Button from '@/(presentation)/shared/components/button.component';
 import { usePasswordMatchChecker } from '@/(presentation)/(pages)/register/hooks/usePasswordMatchChecker';
+import { useEmailRegister } from '@/(presentation)/(pages)/register/hooks/useEmailRegister';
+import Checkbox from '@/(presentation)/shared/components/checkbox.component';
+import Link from 'next/link';
 
 function EmailRegister() {
     const { isMatch, onPasswordChangeHanlder, onConfirmPasswordChangeHanlder } =
@@ -32,6 +35,18 @@ function EmailRegister() {
                 onChange={onConfirmPasswordChangeHanlder}
                 error={!isMatch ? '비밀번호가 일치하지 않습니다.' : undefined}
             />
+
+            <Checkbox name="terms" required>
+                (필수) 서비스 이용약관에 동의합니다.{' '}
+                <Link
+                    className={'opacity-70 hover:opacity-100'}
+                    href={'/'}
+                    target={'_blank'}
+                >
+                    [약관 보기]
+                </Link>
+            </Checkbox>
+
             <Button
                 type={'submit'}
                 className={'w-full'}
