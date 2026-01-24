484b2ff3c8f52fd1f07bf5a85065e4d9b2e47682
y0ngha <dev.y0ngha@gmail.com>
2026-01-24 22:41:30 +0900

    feat: 전략 Dehydrate 진행하는 컴포넌트 추가.

---
+++ app/dehydrate-components/strategies-dehydrate.component.tsx
@@ -0,0 +1,45 @@
+import { ReactNode } from 'react';
+import { getQueryClient } from '@/(presentation)/shared/helpers/query-client.helpers';
+import { getOwnedStrategiesAction } from '@/(presentation)/strategy/actions/get-owned-strategies.action';
+import { ReactQueryKeys } from '@/(presentation)/shared/constants/react-query-keys';
+import { getSharedStrategiesAction } from '@/(presentation)/strategy/actions/get-shared-strategies.action';
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+import { cookies } from 'next/headers';
+
+export default async function StrategiesDehydrate({
+    children,
+}: {
+    children: ReactNode;
+}) {
+    const cookieStore = await cookies();
+    const userId = cookieStore.get('userId')?.value;
+
+    if (!userId) {
+        return children;
+    }
+
+    const queryClient = getQueryClient();
+
+    await Promise.all([
+        queryClient.prefetchInfiniteQuery({
+            queryKey: [userId, ReactQueryKeys.STRATIGES],
+            queryFn: async ({ pageParam }) =>
+                await getOwnedStrategiesAction(userId, pageParam, 15),
+            initialPageParam: 1,
+        }),
+        queryClient.prefetchInfiniteQuery({
+            queryKey: [userId, ReactQueryKeys.SHARED_STRATIGES],
+            queryFn: async ({ pageParam }) =>
+                await getSharedStrategiesAction(userId, pageParam, 15),
+            initialPageParam: 1,
+        }),
+    ]);
+
+    const dehydratedState = dehydrate(queryClient);
+
+    return (
+        <HydrationBoundary state={dehydratedState}>
+            {children}
+        </HydrationBoundary>
+    );
+}
