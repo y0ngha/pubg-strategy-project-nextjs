5c5095bb3765f406aac984ee1b9e48657a3a0a52
y0ngha <dev.y0ngha@gmail.com>
2026-01-24 22:41:20 +0900

    feat: userId CookieKeys에 추가.

--- app/application/constants/cookie-keys.ts
+++ app/application/constants/cookie-keys.ts
@@ -1,6 +1,7 @@
 export const CookieKeys = {
     ACCESS_TOKEN: 'ACCESS_TOKEN',
     REFRESH_TOKEN: 'REFRESH_TOKEN',
+    USER_ID: 'USER_ID',
 } as const;
 
 export type CookieKeys = (typeof CookieKeys)[keyof typeof CookieKeys];
