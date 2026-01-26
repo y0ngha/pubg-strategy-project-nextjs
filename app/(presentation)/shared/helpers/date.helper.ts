8deaf94d9b4f771f646c540f0a22cd81578140c4
y0ngha <dev.y0ngha@gmail.com>
2026-01-26 21:36:54 +0900

    feat: Date 변환 Helpers 함수 작성.

---
+++ app/(presentation)/shared/helpers/date.helper.ts
@@ -0,0 +1,18 @@
+export function toYyyyMmDdString(_date: Date, seperator: string = '-') {
+    const year = _date.getFullYear().toString();
+    const month = (_date.getMonth() + 1).toString().padStart(2, '0');
+    const date = _date.getDate().toString().padStart(2, '0');
+
+    return `${year}${seperator}${month}${date}`;
+}
+
+export function toYyyyMmDdHhMmString(
+    _date: Date,
+    dateSeperator: string = '-',
+    timeSeperator: string = ':'
+) {
+    const hour = _date.getHours().toString().padStart(2, '0');
+    const minute = _date.getMinutes().toString().padStart(2, '0');
+
+    return `${toYyyyMmDdString(_date, dateSeperator)} ${hour}${timeSeperator}${minute}`;
+}
