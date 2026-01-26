df365fb85f23ae2c680bd9d89afb1840b56073da
y0ngha <dev.y0ngha@gmail.com>
2026-01-26 20:21:57 +0900

    feat: 전략 조회시 다음 페이지 여부와 함께 Data 리턴하도록 수정 및 전략 조회 Use Case에서 정렬하는 로직 제거. - 정렬은 백엔드에서 내려준 것을 그대로 의존함.

--- app/application/strategy/use-cases/get-shared-strategies.usecase.ts
+++ app/application/strategy/use-cases/get-shared-strategies.usecase.ts
@@ -5,7 +5,6 @@ import {
     GetStrategiesRequestSchema,
 } from '@/application/strategy/dto/strategy/get-strategy.dto';
 import { StrategyMapper } from '@/application/strategy/mappers/strategy.mapper';
-import { sortingByNewestAndTitle } from '@/application/strategy/use-cases/helpers/strategy.helper';
 
 @injectable()
 export class GetSharedStrategiesUseCase {
@@ -19,15 +18,18 @@ export class GetSharedStrategiesUseCase {
     async execute(dto: GetStrategiesRequestDto) {
         const { actorId, page, limit } = GetStrategiesRequestSchema.parse(dto);
 
-        const strategies =
+        const { hasNextPage, data } =
             await this.strategyRepository.findSharedStrategiesByUserID(
                 actorId,
                 page,
                 limit
             );
 
-        return strategies
-            .map(strategy => this.strategyMapper.toResponse(strategy, actorId))
-            .sort(sortingByNewestAndTitle);
+        return {
+            hasNextPage: hasNextPage,
+            data: data.map(strategy =>
+                this.strategyMapper.toResponse(strategy, actorId)
+            ),
+        };
     }
 }
