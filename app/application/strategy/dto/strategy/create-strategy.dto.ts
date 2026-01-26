28c05ccbffa42923c1da1db8a19fe2604f37c7de
y0ngha <dev.y0ngha@gmail.com>
2026-01-26 20:24:15 +0900

    feat: 전략 Entity에서 OwnerEmail 추가됨에 따라 전략 생성 Dto에서 actorEmail 받도록 추가.

--- app/application/strategy/dto/strategy/create-strategy.dto.ts
+++ app/application/strategy/dto/strategy/create-strategy.dto.ts
@@ -2,9 +2,11 @@ import { z } from 'zod';
 import { UserId } from '@domain/shared/value-objects/user-id';
 import { PubgMap } from '@domain/strategy/enums/map.enum';
 import { StrategyTitle } from '@domain/strategy/value-objects/strategy-title';
+import { Email } from '@domain/shared/value-objects/email';
 
 export interface CreateStrategyRequestDto {
     actorId: string;
+    actorEmail: string;
     title: string;
     map: string;
 }
@@ -12,6 +14,9 @@ export interface CreateStrategyRequestDto {
 export const CreateStrategyRequestSchema = z.object({
     actorId: z.string().transform(value => {
         return UserId.create(value);
+    }),
+    actorEmail: z.string().transform(value => {
+        return Email.create(value);
     }),
     title: z.string().transform(value => {
         return StrategyTitle.create(value);
