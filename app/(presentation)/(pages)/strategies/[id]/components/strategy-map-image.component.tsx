677250280297fff39a35ad1ff859835344b871eb
y0ngha <dev.y0ngha@gmail.com>
2026-01-29 21:36:57 +0900

    feat: 전략 맵 이미지 컴포넌트에서 맵 불러오는동안 스켈레톤 컴포넌트 띄우도록 수정.

--- app/(presentation)/(pages)/strategies/[id]/components/strategy-map-image.component.tsx
+++ app/(presentation)/(pages)/strategies/[id]/components/strategy-map-image.component.tsx
@@ -2,19 +2,25 @@
 
 import { Image } from 'react-konva';
 import useImage from 'use-image';
+import StrategyMapImageSkeleton from '@/(presentation)/(pages)/strategies/[id]/components/strategy-map-image-skeleton.component';
 
 interface StrategyMapImageProps {
     src: string;
 }
 
 function StrategyMapImage({ src }: StrategyMapImageProps) {
-    const [image] = useImage(src);
+    const [image, status] = useImage(src);
+    const originalMapSize = 8192;
+
+    if (status === 'loading') {
+        return <StrategyMapImageSkeleton text={'맵 데이터를 불러오는 중...'} />;
+    }
 
     return (
         <Image
             image={image}
-            width={8192}
-            height={8192}
+            width={originalMapSize}
+            height={originalMapSize}
             listening={false}
             alt={'배틀그라운드 맵 이미지'}
         />
