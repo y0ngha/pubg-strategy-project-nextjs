df261d72ca7c48dfb1ac5f16590a5fa6c5e68e9f
y0ngha <dev.y0ngha@gmail.com>
2026-01-29 20:01:14 +0900

    refactor: 전략 map-canvas 컴포넌트 삭제 후 canvas가 대체하도록 수정(역할 중복이라 하나 삭제).

--- app/(presentation)/(pages)/strategies/[id]/components/strategy-canvas.component.tsx
+++ app/(presentation)/(pages)/strategies/[id]/components/strategy-canvas.component.tsx
@@ -1,10 +1,64 @@
 'use client';
 
-import { PubgMap } from '@domain/strategy/enums/map.enum';
-import StrategyMapCanvas from '@/(presentation)/(pages)/strategies/[id]/components/strategy-map-canvas.component';
+import { CanvasTool } from '@/(presentation)/(pages)/strategies/[id]/components/hooks/useToolbar';
+import { ReactNode, useRef, useState } from 'react';
+import { Layer, Stage } from 'react-konva';
+import { useResizeObserver } from '@/(presentation)/(pages)/strategies/[id]/components/hooks/useResizeObserver';
+import { useKonvaHandleWheelZoomControl } from '@/(presentation)/(pages)/strategies/[id]/components/hooks/useKonvaHandleWheelZoomControl';
+import { useKovnaHandleDrag } from '@/(presentation)/(pages)/strategies/[id]/components/hooks/useKovnaHandleDrag';
 
-function StrategyCanvas() {
-    return <StrategyMapCanvas map={PubgMap.ERANGEL} />;
+interface StrategyCanvasProps {
+    selectedTool: CanvasTool;
+    map: ReactNode;
+    properties: ReactNode;
+}
+
+function StrategyCanvas({
+    selectedTool,
+    map,
+    properties,
+}: StrategyCanvasProps) {
+    const containerRef = useRef<HTMLDivElement>(null);
+
+    const [stagePosistion, setStagePosistion] = useState({ x: 0, y: 0 });
+
+    const { width, height } = useResizeObserver(containerRef);
+
+    const { scale, handleWheel } = useKonvaHandleWheelZoomControl();
+
+    const { handleDragBound, handleDragEnd } = useKovnaHandleDrag(scale, {
+        width,
+        height,
+    });
+
+    return (
+        <div
+            className={'h-full w-full overflow-hidden bg-zinc-900'}
+            ref={containerRef}
+        >
+            <Stage
+                width={width}
+                height={height}
+                draggable={true}
+                dragBoundFunc={handleDragBound}
+                onWheel={event => {
+                    const newStagePosition = handleWheel(event);
+                    setStagePosistion(newStagePosition);
+                }}
+                onDragEnd={event => {
+                    const newStagePosition = handleDragEnd(event);
+                    setStagePosistion(newStagePosition);
+                }}
+                scaleX={scale}
+                scaleY={scale}
+                x={stagePosistion.x}
+                y={stagePosistion.y}
+            >
+                <Layer imageSmoothingEnabled={true}>{map}</Layer>
+                <Layer>{properties}</Layer>
+            </Stage>
+        </div>
+    );
 }
 
 StrategyCanvas.displayName = 'StrategyCanvas';
