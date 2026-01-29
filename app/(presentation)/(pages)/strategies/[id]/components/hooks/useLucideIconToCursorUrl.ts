f31bab60e2bfc6a64965bd95935f761714f1ae6f
y0ngha <dev.y0ngha@gmail.com>
2026-01-29 19:59:41 +0900

    feat: lucide icon을 cursor url로 변경해주는 커스텀 훅 작성.

---
+++ app/(presentation)/(pages)/strategies/[id]/components/hooks/useLucideIconToCursorUrl.ts
@@ -0,0 +1,43 @@
+import { LucideIcon } from 'lucide-react';
+import { renderToStaticMarkup } from 'react-dom/server';
+import { createElement } from 'react';
+
+interface CursorOptions {
+    color: string;
+    size: number;
+    strokeWidth: number;
+}
+
+export function useLucideIconToCursorUrl(
+    lucideIcon: LucideIcon,
+    options: CursorOptions = {
+        color: '#ff8c00',
+        size: 28,
+        strokeWidth: 1,
+    }
+) {
+    const getSvgCursorUrl = (iconComponent: LucideIcon): string | undefined => {
+        const svgString = renderToStaticMarkup(
+            createElement(iconComponent, {
+                color: options.color,
+                size: options.size,
+                strokeWidth: options.strokeWidth,
+            })
+        );
+
+        const encodedSvg =
+            typeof window === 'undefined'
+                ? Buffer.from(svgString).toString('base64')
+                : window.btoa(
+                      decodeURIComponent(encodeURIComponent(svgString))
+                  );
+
+        const center = options?.size / 2;
+
+        return `url('data:image/svg+xml;base64,${encodedSvg}') ${center} ${center}, auto`;
+    };
+
+    return {
+        url: getSvgCursorUrl(lucideIcon),
+    };
+}
