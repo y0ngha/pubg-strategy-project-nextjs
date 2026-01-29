280b65cb2507018926eee6a394a7abae8a541179
y0ngha <dev.y0ngha@gmail.com>
2026-01-29 21:40:10 +0900

    refactor: toolbar, canvas 담고 있던 page-content 컴포넌트를 페이지의 내용만 담도록 수정.

--- app/(presentation)/(pages)/strategies/[id]/components/strategy-page-content.component.tsx
+++ app/(presentation)/(pages)/strategies/[id]/components/strategy-page-content.component.tsx
@@ -1,28 +1,14 @@
 'use client';
 
-import StrategyToolbar from '@/(presentation)/(pages)/strategies/[id]/components/strategy-toolbar.component';
-import StrategyCanvas from '@/(presentation)/(pages)/strategies/[id]/components/strategy-canvas.component';
-import { useToolbar } from '@/(presentation)/(pages)/strategies/[id]/components/hooks/useToolbar';
-import { useLucideIconToCursorUrl } from '@/(presentation)/(pages)/strategies/[id]/components/hooks/useLucideIconToCursorUrl';
-import StrategyMapImage from '@/(presentation)/(pages)/strategies/[id]/components/strategy-map-image.component';
 import { useGetStrategy } from '@/(presentation)/(pages)/strategies/[id]/components/hooks/useGetStrategy';
+import StrategyBody from '@/(presentation)/(pages)/strategies/[id]/components/strategy-body.compoenent';
+import StrategyHeader from '@/(presentation)/(pages)/strategies/[id]/components/strategy-header.component';
 
 interface StrategyPageContentProps {
     id: string;
 }
 
 function StrategyPageContent({ id }: StrategyPageContentProps) {
-    const {
-        canvasToolGroup,
-        canvasToolNames,
-        canvasToolIcons,
-        changeTool,
-        selectedTool,
-        iconSize,
-    } = useToolbar();
-
-    const { url } = useLucideIconToCursorUrl(canvasToolIcons[selectedTool]);
-
     const { data: strategy } = useGetStrategy(id);
 
     if (!strategy) {
@@ -30,20 +16,9 @@ function StrategyPageContent({ id }: StrategyPageContentProps) {
     }
 
     return (
-        <div className={'flex h-full w-full flex-row'} style={{ cursor: url }}>
-            <StrategyToolbar
-                canvasToolGroup={canvasToolGroup}
-                canvasToolNames={canvasToolNames}
-                canvasToolIcons={canvasToolIcons}
-                changeTool={changeTool}
-                selectedTool={selectedTool}
-                iconSize={iconSize}
-            />
-            <StrategyCanvas
-                selectedTool={selectedTool}
-                map={<StrategyMapImage src={strategy.mapImage} />}
-                properties={<></>}
-            />
+        <div className={'flex h-full w-full flex-col'}>
+            <StrategyHeader id={id} title={strategy.title} />
+            <StrategyBody id={id} mapImage={strategy.mapImage} />
         </div>
     );
 }
