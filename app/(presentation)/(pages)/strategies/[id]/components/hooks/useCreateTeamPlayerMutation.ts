ecd54a38bddb0ab9a3c217d155d05634f3eb4c8a
y0ngha <dev.y0ngha@gmail.com>
2026-01-30 17:43:01 +0900

    feat: 전략 팀 플레이어 생성 Mutation 작성.

---
+++ app/(presentation)/(pages)/strategies/[id]/components/hooks/useCreateTeamPlayerMutation.ts
@@ -0,0 +1,59 @@
+import { getQueryClient } from '@/(presentation)/shared/helpers/query-client.helpers';
+import { useMutation } from '@tanstack/react-query';
+import { addTeamPlayerAction } from '@/(presentation)/strategy/actions/team-player/add-team-player.action';
+import { useGetCurrentUser } from '@/(presentation)/shared/hooks/useGetCurrentUser';
+import { toast } from 'react-toastify';
+import { ReactQueryKeys } from '@/(presentation)/shared/constants/react-query-keys';
+import { GetStrategyAction } from '@/(presentation)/strategy/actions/get-strategy.action';
+
+export function useCreateTeamPlayerMutation(strategyId: string) {
+    const queryClient = getQueryClient();
+    const user = useGetCurrentUser();
+
+    const { mutate } = useMutation({
+        mutationFn: async () => {
+            const formData = new FormData();
+
+            formData.set('userId', user.data?.id ?? '');
+            formData.set('strategyId', strategyId);
+
+            return await addTeamPlayerAction(formData);
+        },
+        onSuccess: data => {
+            const strataegyQueryKey = [ReactQueryKeys.STRATIGES, strategyId];
+
+            queryClient.setQueryData<GetStrategyAction>(
+                strataegyQueryKey,
+                oldStrategy => {
+                    if (!oldStrategy) {
+                        return undefined;
+                    }
+
+                    return {
+                        ...oldStrategy,
+                        teamPlayers: [
+                            ...oldStrategy.teamPlayers,
+                            {
+                                id: data.id,
+                                priority: data.priority,
+                                position: data.position,
+                                color: data.color,
+                            },
+                        ],
+                    };
+                }
+            );
+        },
+        onError: error => {
+            console.error('useCreateTeamPlayerMutation', error);
+            toast.error(
+                error.message ??
+                    '알 수 없는 오류로 팀 플레이어 생성에 실패했습니다.'
+            );
+        },
+    });
+
+    return {
+        createTeamPlayer: mutate,
+    };
+}
