8cc983b9a47f7f82a03871ac7c729dd1dfa53ddf
y0ngha <dev.y0ngha@gmail.com>
2026-01-30 17:55:03 +0900

    feat: 비행기 동선 생성 Mutation 작성.

---
+++ app/(presentation)/(pages)/strategies/[id]/components/hooks/useCreateAirplanePathMutation.ts
@@ -0,0 +1,57 @@
+import { getQueryClient } from '@/(presentation)/shared/helpers/query-client.helpers';
+import { useMutation } from '@tanstack/react-query';
+import { useGetCurrentUser } from '@/(presentation)/shared/hooks/useGetCurrentUser';
+import { addAirplanePathAction } from '@/(presentation)/strategy/actions/airplane-path/add-airplane-path.action';
+import { ReactQueryKeys } from '@/(presentation)/shared/constants/react-query-keys';
+import { GetStrategyAction } from '@/(presentation)/strategy/actions/get-strategy.action';
+import { toast } from 'react-toastify';
+
+export function useCreateAirplanePathMutation(strategyId: string) {
+    const queryClient = getQueryClient();
+    const user = useGetCurrentUser();
+
+    const { mutate } = useMutation({
+        mutationFn: async (formData: FormData) => {
+            formData.set('userId', user.data?.id ?? '');
+            formData.set('strategyId', strategyId);
+
+            return await addAirplanePathAction(formData);
+        },
+        onSuccess: data => {
+            const strataegyQueryKey = [ReactQueryKeys.STRATIGES, strategyId];
+
+            queryClient.invalidateQueries({
+                queryKey: [user.data?.id, ReactQueryKeys.STRATIGES],
+            });
+
+            queryClient.setQueryData<GetStrategyAction>(
+                strataegyQueryKey,
+                oldStrategy => {
+                    if (!oldStrategy) {
+                        return undefined;
+                    }
+
+                    return {
+                        ...oldStrategy,
+                        airplanePath: {
+                            id: data.id,
+                            startPosition: data.startPosition,
+                            endPosition: data.endPosition,
+                        },
+                    };
+                }
+            );
+        },
+        onError: error => {
+            console.error('useCreateAirplanePathMutation', error);
+            toast.error(
+                error.message ??
+                    '알 수 없는 오류로 비행기 동선 생성에 실패했습니다.'
+            );
+        },
+    });
+
+    return {
+        createAirplanePath: mutate,
+    };
+}
