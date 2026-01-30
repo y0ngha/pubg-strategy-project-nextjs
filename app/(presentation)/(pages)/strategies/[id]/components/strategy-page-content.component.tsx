a5974fe897f1db1d73e75a11773356cf77108b70
y0ngha <dev.y0ngha@gmail.com>
2026-01-30 17:35:08 +0900

    feat: 전략 페이지에서 전략 불러올 때 불러와지는 동안 스켈레톤 노출되도록 수정.

--- app/(presentation)/(pages)/strategies/[id]/components/strategy-page-content.component.tsx
+++ app/(presentation)/(pages)/strategies/[id]/components/strategy-page-content.component.tsx
@@ -3,22 +3,24 @@
 import { useGetStrategy } from '@/(presentation)/(pages)/strategies/[id]/components/hooks/useGetStrategy';
 import StrategyBody from '@/(presentation)/(pages)/strategies/[id]/components/strategy-body.compoenent';
 import StrategyHeader from '@/(presentation)/(pages)/strategies/[id]/components/strategy-header.component';
+import Skeleton from '@/(presentation)/shared/components/skeleton.component';
 
 interface StrategyPageContentProps {
     id: string;
 }
 
 function StrategyPageContent({ id }: StrategyPageContentProps) {
-    const { data: strategy } = useGetStrategy(id);
-
-    if (!strategy) {
-        return <></>;
-    }
+    const { data: strategy, isPending } = useGetStrategy(id);
 
     return (
         <div className={'flex h-full w-full flex-col'}>
-            <StrategyHeader id={id} title={strategy.title} />
-            <StrategyBody id={id} mapImage={strategy.mapImage} />
+            {!strategy && isPending && <Skeleton className={'h-full'} />}
+            {strategy && (
+                <>
+                    <StrategyHeader id={id} title={strategy.title} />
+                    <StrategyBody id={id} mapImage={strategy.mapImage} />
+                </>
+            )}
         </div>
     );
 }
