1f1c1595cd0108400d36a55d2f290f247782def6
y0ngha <dev.y0ngha@gmail.com>
2026-01-31 20:59:21 +0900

    feat: 맵 클릭 연결 및 자기장 생성을 위한 흐름 연결.

--- app/(presentation)/(pages)/strategies/[id]/components/strategy-body.compoenent.tsx
+++ app/(presentation)/(pages)/strategies/[id]/components/strategy-body.compoenent.tsx
@@ -1,9 +1,7 @@
 'use client';
 
 import { useToolbar } from '@/(presentation)/(pages)/strategies/[id]/components/hooks/tools/useToolbar';
-import {
-    useLucideIconToCursorUrl
-} from '@/(presentation)/(pages)/strategies/[id]/components/hooks/utils/useLucideIconToCursorUrl';
+import { useLucideIconToCursorUrl } from '@/(presentation)/(pages)/strategies/[id]/components/hooks/utils/useLucideIconToCursorUrl';
 import StrategyToolbar from '@/(presentation)/(pages)/strategies/[id]/components/strategy-toolbar.component';
 import StrategyCanvas from '@/(presentation)/(pages)/strategies/[id]/components/strategy-canvas.component';
 import StrategyMapImage from '@/(presentation)/(pages)/strategies/[id]/components/strategy-map-image.component';
@@ -13,6 +11,8 @@ import CircleProperty from '@/(presentation)/(pages)/strategies/[id]/components/
 import { Layer } from 'react-konva';
 import { useCircleEvent } from '@/(presentation)/(pages)/strategies/[id]/components/hooks/tools/useCircleEvent';
 import { CircleResponseDto } from '@/application/strategy/dto/strategy/get-strategy.dto';
+import PhaseSelectModal from '@/(presentation)/(pages)/strategies/[id]/components/modals/phase-select.modal';
+import { useKonvaHandleMouseClick } from '@/(presentation)/(pages)/strategies/[id]/components/hooks/konvas/useKonvaHandleMouseClick';
 
 interface StrategyBodyProps {
     id: string;
@@ -21,6 +21,22 @@ interface StrategyBodyProps {
     handleMouseMove: () => void;
     mousePosition: { x: number; y: number };
     circles: CircleResponseDto[];
+}
+
+function CirclesLayer({ circles }: Pick<StrategyBodyProps, 'circles'>) {
+    return (
+        <Layer>
+            {circles.map(field => (
+                <CircleProperty
+                    key={field.phase}
+                    color={field.color}
+                    radius={field.radius}
+                    x={field.centerPosition.x}
+                    y={field.centerPosition.y}
+                />
+            ))}
+        </Layer>
+    );
 }
 
 function StrategyBody({
@@ -42,6 +58,27 @@ function StrategyBody({
 
     const { url } = useLucideIconToCursorUrl(canvasToolIcons[selectedTool]);
 
+    const {
+        isPhaseSelectModalOpen,
+        phaseSelectModalOpen,
+        phaseSelectModalClose,
+        onCircleCreateConfirm,
+    } = useCircleEvent(id);
+
+    const onMapClick = (clickPosition: { x: number; y: number }) => {
+        switch (selectedTool) {
+            case 'circle':
+                return phaseSelectModalOpen(clickPosition);
+        }
+    };
+
+    const { handleClick } = useKonvaHandleMouseClick(
+        mousePosition,
+        (_, clickPosition) => {
+            onMapClick(clickPosition);
+        }
+    );
+
     return (
         <div className={'flex h-full flex-1 flex-row'} style={{ cursor: url }}>
             <StrategyToolbar
@@ -55,10 +92,16 @@ function StrategyBody({
             <StrategyCanvas
                 stageRef={stageRef}
                 handleMouseMove={handleMouseMove}
-                mousePosition={mousePosition}
                 selectedTool={selectedTool}
                 map={<StrategyMapImage src={mapImage} />}
-                properties={<></>}
+                properties={<CirclesLayer circles={circles} />}
+                onMapClick={handleClick}
+            />
+
+            <PhaseSelectModal
+                isOpen={isPhaseSelectModalOpen}
+                onClose={phaseSelectModalClose}
+                onConfirm={onCircleCreateConfirm}
             />
         </div>
     );
