69228ec1f093179493506b6ad7af785782096acd
y0ngha <dev.y0ngha@gmail.com>
2026-01-31 20:56:14 +0900

    feat: Konva 마우스 클릭 커스텀 훅에서 맵 클릭 포지션 관리하도록 수정.

--- app/(presentation)/(pages)/strategies/[id]/components/hooks/konvas/useKonvaHandleMouseClick.ts
+++ app/(presentation)/(pages)/strategies/[id]/components/hooks/konvas/useKonvaHandleMouseClick.ts
@@ -1,4 +1,5 @@
 import { KonvaEventObject } from 'konva/lib/Node';
+import { useState } from 'react';
 
 export function useKonvaHandleMouseClick(
     mousePosition: { x: number; y: number },
@@ -7,11 +8,18 @@ export function useKonvaHandleMouseClick(
         clickPosition: { x: number; y: number }
     ) => void
 ) {
+    const [mapClickPosition, setMapClickPosition] = useState<{
+        x: number;
+        y: number;
+    }>({ x: 0, y: 0 });
+
     const handleClick = (event: KonvaEventObject<MouseEvent>) => {
+        setMapClickPosition(mousePosition);
         onClick?.(event, mousePosition);
     };
 
     return {
         handleClick,
+        mapClickPosition,
     };
 }
