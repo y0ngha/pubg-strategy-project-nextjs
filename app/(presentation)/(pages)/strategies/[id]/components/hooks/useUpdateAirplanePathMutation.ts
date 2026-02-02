365f4223769fa79669ef7bfaeaff3ba64c537d8e
y0ngha <dev.y0ngha@gmail.com>
2026-02-02 17:10:13 +0900

    Revert "refactor: 비행기 동선 업데이트시 성공시에만 캐시 업데이트를 진행하는 것이 아닌, 정말 낙관적인 업데이트를 진행하도록 수정."
    This reverts commit 9af3da75a4b5d1f3d1dd68d110374129f2cbc98f.

--- app/(presentation)/(pages)/strategies/[id]/components/hooks/useUpdateAirplanePathMutation.ts
+++ app/(presentation)/(pages)/strategies/[id]/components/hooks/useUpdateAirplanePathMutation.ts
@@ -9,17 +9,10 @@ import {
     UpdateAirplanePathAction,
     updateAirplanePathAction,
 } from '@/(presentation)/strategy/actions/airplane-path/update-airplane-path.action';
-import { parseFormData } from '@/(presentation)/shared/helpers/form-data.helper';
 
 export function useUpdateAirplanePathMutation(strategyId: string) {
     const queryClient = getQueryClient();
     const user = useGetCurrentUser();
-
-    const strategyQueryKey: QueryKey = [ReactQueryKeys.STRATIGES, strategyId];
-    const strategiesQueryKey: QueryKey = [
-        user.data?.id,
-        ReactQueryKeys.STRATIGES,
-    ];
 
     const { mutate } = useMutation({
         mutationFn: async (formData: FormData) => {
@@ -28,57 +21,19 @@ export function useUpdateAirplanePathMutation(strategyId: string) {
 
             return await updateAirplanePathAction(formData);
         },
-        onMutate: async (formData: FormData) => {
-            await queryClient.cancelQueries({
-                queryKey: strategyQueryKey,
+        onSuccess: data => {
+            optimisticUpdate([ReactQueryKeys.STRATIGES, strategyId], data);
+
+            queryClient.invalidateQueries({
+                queryKey: [user.data?.id, ReactQueryKeys.STRATIGES],
             });
-
-            const previousStrategy =
-                queryClient.getQueryData<GetStrategyAction>(strategyQueryKey);
-
-            const { startPosition, endPosition } = parseFormData(formData, [
-                {
-                    key: 'startPosition',
-                    type: 'position',
-                    allowUndefined: true,
-                },
-                {
-                    key: 'endPosition',
-                    type: 'position',
-                    allowUndefined: true,
-                },
-            ] as const);
-
-            if (startPosition && endPosition) {
-                optimisticUpdate(strategyQueryKey, {
-                    startPosition,
-                    endPosition,
-                });
-            }
-
-            return { previousStrategy };
         },
-        onError: (error, variables, context) => {
-            if (context?.previousStrategy) {
-                queryClient.setQueryData(
-                    strategyQueryKey,
-                    context.previousStrategy
-                );
-            }
-
-            console.error('useUpdateAirplanePathMutation', error, variables);
+        onError: error => {
+            console.error('useUpdateAirplanePathMutation', error);
             toast.error(
                 error.message ??
                     '알 수 없는 오류로 비행기 동선 수정에 실패했습니다.'
             );
-        },
-        onSettled: () => {
-            queryClient.invalidateQueries({
-                queryKey: strategiesQueryKey,
-            });
-            queryClient.invalidateQueries({
-                queryKey: strategyQueryKey,
-            });
         },
     });
 
