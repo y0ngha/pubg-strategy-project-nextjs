15ef24d4496079a001bd7e0ec3665c6bfd9fe942
y0ngha <dev.y0ngha@gmail.com>
2026-02-03 20:02:27 +0900

    feat: konvaMouseClick 커스텀훅에 맵 클릭 포지션 외에도 윈도우 포지션도 받을 수 있도록 수정.

--- app/(presentation)/(pages)/strategies/[id]/components/hooks/konvas/useKonvaHandleMouseClick.ts
+++ app/(presentation)/(pages)/strategies/[id]/components/hooks/konvas/useKonvaHandleMouseClick.ts
@@ -3,18 +3,22 @@ import { KonvaEventObject } from 'konva/lib/Node';
 export function useKonvaHandleMouseClick(
     onClick?: (
         event: KonvaEventObject<MouseEvent>,
-        clickPosition: { x: number; y: number }
+        clickPosition: { x: number; y: number },
+        windowPosition: { x: number; y: number }
     ) => void
 ) {
     const handleClick = (event: KonvaEventObject<MouseEvent>) => {
         const mousePosition = event.target
             .getStage()
             ?.getRelativePointerPosition();
+
         if (!mousePosition) {
             return;
         }
 
-        onClick?.(event, mousePosition);
+        const windowPosition = { x: event.evt.clientX, y: event.evt.clientY };
+
+        onClick?.(event, mousePosition, windowPosition);
     };
 
     return {
