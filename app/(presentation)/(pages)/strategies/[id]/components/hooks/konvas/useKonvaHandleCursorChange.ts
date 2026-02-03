0cc0f6ff2cf6943ad02e2990e26732c9964fd287
y0ngha <dev.y0ngha@gmail.com>
2026-02-03 18:05:23 +0900

    refactor: useKonvaHandleCursorChange 사용시 변경할 커서와 기본 커서 파라미터로 받도록 수정.

--- app/(presentation)/(pages)/strategies/[id]/components/hooks/konvas/useKonvaHandleCursorChange.ts
+++ app/(presentation)/(pages)/strategies/[id]/components/hooks/konvas/useKonvaHandleCursorChange.ts
@@ -1,14 +1,20 @@
 import { KonvaEventObject } from 'konva/lib/Node';
 import { useState } from 'react';
+import { Property } from 'csstype';
 
-export function useKonvaHandleCursorChange() {
-    const [previousCursor, setPreviousCursor] = useState<string>('default');
+type Cursor = Property.Cursor;
+
+export function useKonvaHandleCursorChange(
+    changeCursor: Cursor,
+    defaultCursor: Cursor = 'default'
+) {
+    const [previousCursor, setPreviousCursor] = useState<string>(defaultCursor);
 
     const handleMouseEnter = (e: KonvaEventObject<MouseEvent>) => {
         const stage = e.target.getStage();
         if (stage) {
             setPreviousCursor(stage.container().style.cursor);
-            stage.container().style.cursor = 'pointer';
+            stage.container().style.cursor = changeCursor;
         }
     };
 
