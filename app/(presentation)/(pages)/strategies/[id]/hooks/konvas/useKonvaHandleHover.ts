726ee053129e08a7cb4a162fb569d614ea8fac90
y0ngha <dev.y0ngha@gmail.com>
2026-02-04 19:54:24 +0900

    feat: konva handle hover 진행시 마우스 커서 자동으로 변경하고, property 값 변경되는 것들에 대해 관리 진행.

--- app/(presentation)/(pages)/strategies/[id]/hooks/konvas/useKonvaHandleHover.ts
+++ app/(presentation)/(pages)/strategies/[id]/hooks/konvas/useKonvaHandleHover.ts
@@ -1,15 +1,39 @@
 import { useState } from 'react';
+import { useKonvaHandleCursorChange } from '@/(presentation)/(pages)/strategies/[id]/hooks/konvas/useKonvaHandleCursorChange';
+import { KonvaEventObject } from 'konva/lib/Node';
 
-export function useKonvaHandleHover() {
+export function useKonvaHandleHover(baseColor: string) {
+    const {
+        handleMouseLeave: cursorHandleMouseLeave,
+        handleMouseEnter: cursorHandleMouseEnter,
+    } = useKonvaHandleCursorChange('pointer', 'default');
+
     const [isHovered, setIsHovered] = useState<boolean>(false);
 
-    const handleMouseEnter = () => {
+    const handleMouseEnter = (event: KonvaEventObject<MouseEvent>) => {
         setIsHovered(true);
+        cursorHandleMouseEnter(event);
     };
 
-    const handleMouseLeave = () => {
+    const handleMouseLeave = (event: KonvaEventObject<MouseEvent>) => {
         setIsHovered(false);
+        cursorHandleMouseLeave(event);
     };
 
-    return { isHovered, handleMouseEnter, handleMouseLeave };
+    const scaleX = isHovered ? 1.2 : 1;
+    const scaleY = isHovered ? 1.2 : 1;
+    const shadowBlur = isHovered ? 15 : 0;
+    const shadowColor = baseColor;
+    const shadowOpacity = 0.8;
+
+    return {
+        isHovered,
+        handleMouseEnter,
+        handleMouseLeave,
+        scaleX,
+        scaleY,
+        shadowBlur,
+        shadowColor,
+        shadowOpacity,
+    };
 }
