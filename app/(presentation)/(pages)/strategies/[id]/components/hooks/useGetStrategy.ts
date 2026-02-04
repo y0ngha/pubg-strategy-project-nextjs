b8d743967a39cad951938e82acfae7baf81cc5ca
y0ngha <dev.y0ngha@gmail.com>
2026-02-05 01:29:59 +0900

    revert: useGetStrategy 테스트파일 올라간 부분 복원 처리.

--- app/(presentation)/(pages)/strategies/[id]/components/hooks/useGetStrategy.ts
+++ app/(presentation)/(pages)/strategies/[id]/components/hooks/useGetStrategy.ts
@@ -4,6 +4,7 @@ import {
     GetStrategyAction,
     getStrategyAction,
 } from '@/(presentation)/strategy/actions/get-strategy.action';
+import { useGetCurrentUser } from '@/(presentation)/shared/hooks/useGetCurrentUser';
 
 export function useGetStrategy(
     strategyId: string,
@@ -12,25 +13,20 @@ export function useGetStrategy(
         'queryKey' | 'queryFn' | 'staleTime' | 'gcTime' | 'retry' | 'enabled'
     >
 ) {
-    // TODO TEST중. enabled부터 해서 각종 내용 업데이트 필요
+    const user = useGetCurrentUser();
 
-    // const user = useGetCurrentUser();
     return useQuery({
         queryKey: [ReactQueryKeys.STRATIGES, strategyId],
         queryFn: async () => {
-            // if (user.data?.id === undefined) {
-            //     return Promise.reject(
-            //         new Error('유저 고유 식별자를 불러오지 못했습니다.')
-            //     );
-            // }
+            if (user.data?.id === undefined) {
+                return Promise.reject(
+                    new Error('유저 고유 식별자를 불러오지 못했습니다.')
+                );
+            }
 
-            // return await getStrategyAction(user.data.id, strategyId);
-            return await getStrategyAction(
-                '550e8400-e29b-41d4-a716-446655440000',
-                strategyId
-            );
+            return await getStrategyAction(user.data.id, strategyId);
         },
-        // enabled: user.data?.id !== undefined && !!strategyId,
+        enabled: user.data?.id !== undefined && !!strategyId,
         staleTime: 1000 * 60 * 60,
         gcTime: 1000 * 60 * 60 * 24,
         retry: false,
