cbdc6133957548d1ed25ce777ce4456eded1504d
y0ngha <dev.y0ngha@gmail.com>
2026-02-04 22:54:40 +0900

    feat: 적 팀 움직일 때 deltaPosition을 받아 커스텀훅 내부에서 계산하도록 수정.

--- app/(presentation)/(pages)/strategies/[id]/hooks/tools/properties/useEnemyTeamEvent.ts
+++ app/(presentation)/(pages)/strategies/[id]/hooks/tools/properties/useEnemyTeamEvent.ts
@@ -1,8 +1,12 @@
 import { useCreateEnemyTeamMutation } from '@/(presentation)/(pages)/strategies/[id]/hooks/mutations/create/useCreateEnemyTeamMutation';
 import { useState } from 'react';
 import { useUpdateEnemyTeamMutation } from '@/(presentation)/(pages)/strategies/[id]/hooks/mutations/update/useUpdateEnemyTeamMutation';
+import { EnemyTeamResponseDto } from '@/application/strategy/dto/strategy/get-strategy.dto';
 
-export function useEnemyTeamEvent(strategyId: string) {
+export function useEnemyTeamEvent(
+    strategyId: string,
+    enemyTeams: EnemyTeamResponseDto[]
+) {
     const { createEnemyTeam: createEnemyTeamMutation } =
         useCreateEnemyTeamMutation(strategyId);
     const { updateEnemyTeam: updateEnemyTeamMutation } =
@@ -40,8 +44,21 @@ export function useEnemyTeamEvent(strategyId: string) {
 
     const moveEnemyTeam = (
         enemyTeamId: string,
-        position: { x: number; y: number }
+        deltaPosition: { x: number; y: number }
     ) => {
+        const enemyTeam = enemyTeams.find(
+            enemyTeam => enemyTeam.id === enemyTeamId
+        );
+
+        if (!enemyTeam) {
+            throw new Error('적 팀 ID로 적 팀을 찾을 수 없습니다.');
+        }
+
+        const position = {
+            x: enemyTeam.position.x + deltaPosition.x,
+            y: enemyTeam.position.y + deltaPosition.y,
+        };
+
         const formData = new FormData();
         formData.set('enemyTeamId', enemyTeamId);
         formData.set('position', JSON.stringify(position));
