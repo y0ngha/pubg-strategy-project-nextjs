864ca0093494a7ee61ac9a8859eff43424813674
y0ngha <dev.y0ngha@gmail.com>
2026-02-08 18:08:19 +0900

    feat: MyPage > 친구 메뉴를 담고 있는 컴포넌트 작성.

---
+++ app/(presentation)/(pages)/my-page/components/my-page-friend-menu.component.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import MyPageMenuCard from '@/(presentation)/(pages)/my-page/components/my-page-menu-card.component';
+import { Users } from 'lucide-react';
+import { Route } from '@/(presentation)/shared/constants/route';
+import { useGetFriends } from '@/(presentation)/shared/hooks/useGetFriends';
+import { useRouter } from 'next/navigation';
+
+function MyPageFriendMenu() {
+    const { data, isPending } = useGetFriends();
+    const router = useRouter();
+
+    return (
+        <MyPageMenuCard
+            icon={<Users className={'text-blue-600'} />}
+            title={'친구 관리'}
+            description={`함께하는 친구 ${data?.friendCount}명 | 받은 친구 요청 ${data?.receivedFriendRequestCount}명`}
+            isPending={isPending}
+            handleClick={() => router.push(Route.FRIENDS)}
+        />
+    );
+}
+
+MyPageFriendMenu.displayName = 'myPageFriendMenu';
+
+export default MyPageFriendMenu;
