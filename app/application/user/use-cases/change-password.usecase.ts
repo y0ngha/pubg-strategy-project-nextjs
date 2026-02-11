2503d4ae3e50baafeda6fce1f742fcb45efe0205
y0ngha <dev.y0ngha@gmail.com>
2026-02-11 23:30:45 +0900

    refactor: 비밀번호 변경 Use Case COmmand 생성 후 그대로 보내도록 수정.

--- app/application/user/use-cases/change-password.usecase.ts
+++ app/application/user/use-cases/change-password.usecase.ts
@@ -1,102 +1,33 @@
 import { inject, injectable } from 'inversify';
-import { UserRepositoryPort } from '@domain/user/port/out/user-repository.port';
-import { Password } from '@domain/user/value-objects/password';
 import {
     ChangePasswordRequestDto,
     ChangePasswordRequestSchema,
 } from '@/application/user/dto/change-password.dto';
-import { PasswordCipherPort } from '@domain/user/port/out/password-cipher.port';
-import {
-    ChangePasswordException,
-    InvalidPasswordException,
-    UserNotFoundException,
-} from '@domain/user/exceptions/user.exceptions';
-import { Email } from '@domain/shared/value-objects/email';
+import { ChangePasswordCommand } from '@domain/user/commands/change-password.command';
 import { PasswordValidatorPort } from '@domain/user/port/in/password-validator.port';
+import { UserCommandRepositoryPort } from '@domain/user/port/repositories/user-command-repository.port';
 
 @injectable()
 export class ChangePasswordUseCase {
     constructor(
-        @inject(UserRepositoryPort)
-        private readonly userRepository: UserRepositoryPort,
-        @inject(PasswordCipherPort)
-        private readonly passwordCipher: PasswordCipherPort,
+        @inject(UserCommandRepositoryPort)
+        private readonly userCommandRepository: UserCommandRepositoryPort,
         @inject(PasswordValidatorPort)
         private readonly passwordValidator: PasswordValidatorPort
     ) {}
 
     async execute(dto: ChangePasswordRequestDto): Promise<boolean> {
-        const { userId, currentPassword, newPassword } =
+        const { currentPassword, newPassword } =
             ChangePasswordRequestSchema.parse(dto);
 
-        const user = await this.userRepository.findByUserId(userId);
-
-        if (!user) {
-            throw new UserNotFoundException(userId.toString());
-        }
-
-        if (user.password) {
-            this.ensurePasswordMatches(user.password, currentPassword);
-            this.ensurePasswordIsDifferent(currentPassword, newPassword);
-        }
-
-        this.ensurePasswordEmailValidate(user.email, newPassword);
-
-        const encryptedNewPassword = Password.reconstruct(
-            this.passwordCipher.encrypt(newPassword.toString())
+        const command = ChangePasswordCommand.create(
+            currentPassword,
+            newPassword,
+            this.passwordValidator
         );
 
-        user.changePassword(encryptedNewPassword);
+        await this.userCommandRepository.changePassword(command);
 
-        try {
-            await this.userRepository.save(user);
-            return true;
-        } catch (error: unknown) {
-            if (error instanceof Error) {
-                throw new ChangePasswordException(error.message);
-            }
-
-            throw error;
-        }
-    }
-
-    private ensurePasswordEmailValidate(email: Email, password: Password) {
-        if (!this.passwordValidator.emailIncludedValidate(email, password)) {
-            throw new InvalidPasswordException(
-                '신규 비밀번호에 이메일이 포함될 수 없습니다.'
-            );
-        }
-    }
-
-    private ensurePasswordMatches(
-        userPassword: Password,
-        currentPassword: Password
-    ): void {
-        if (
-            !this.passwordValidator.passwordMatchValidate(
-                userPassword,
-                currentPassword
-            )
-        ) {
-            throw new ChangePasswordException(
-                '현재 비밀번호가 일치하지 않습니다.'
-            );
-        }
-    }
-
-    private ensurePasswordIsDifferent(
-        currentPassword: Password,
-        newPassword: Password
-    ): void {
-        if (
-            !this.passwordValidator.passwordDifferentValidate(
-                currentPassword,
-                newPassword
-            )
-        ) {
-            throw new ChangePasswordException(
-                '기존 비밀번호와 새로운 비밀번호는 일치할 수 없습니다.'
-            );
-        }
+        return true;
     }
 }
