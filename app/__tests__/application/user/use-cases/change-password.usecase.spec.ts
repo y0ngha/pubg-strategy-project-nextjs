5dad8ec22f2ac51f3d2cb69953487c1191f28905
y0ngha <dev.y0ngha@gmail.com>
2026-02-12 00:43:26 +0900

    test: 사용하지 않는 파일에 대한 테스트 코드 삭제 및 Use Case 변경건에 대해 반영.

--- app/__tests__/application/user/use-cases/change-password.usecase.spec.ts
+++ app/__tests__/application/user/use-cases/change-password.usecase.spec.ts
@@ -1,103 +1,73 @@
-import { UserRepositoryPort } from '@domain/user/port/out/user-repository.port';
 import { PasswordCipherPort } from '@domain/user/port/out/password-cipher.port';
 import { ChangePasswordUseCase } from '@/application/user/use-cases/change-password.usecase';
-import { ChangePasswordRequestSchema } from '@/application/user/dto/change-password.dto';
-import { User } from '@domain/user/entities/user.entity';
-import { Email } from '@domain/shared/value-objects/email';
-import {
-    ChangePasswordException,
-    InvalidPasswordException,
-} from '@domain/user/exceptions/user.exceptions';
-import { getUserRepositoryMocking } from '@/__tests__/application/helpers/repository-mocking.helpers';
+import { ChangePasswordException } from '@domain/user/exceptions/user.exceptions';
+import { getUserCommandRepositoryMocking } from '@/__tests__/application/helpers/repository-mocking.helpers';
 import {
     getPasswordCipherMocking,
     getPasswordValidatorServiceMocking,
 } from '@/__tests__/application/helpers/service-mocking.helpers';
-import { Password } from '@domain/user/value-objects/password';
 import { PasswordValidatorPort } from '@domain/user/port/in/password-validator.port';
+import { UserCommandRepositoryPort } from '@domain/user/port/repositories/user-command-repository.port';
 
 describe('ChangePasswordUseCase', () => {
     let useCase: ChangePasswordUseCase;
-    let mockUserRepository: jest.Mocked<UserRepositoryPort>;
+    let mockUserCommandRepository: jest.Mocked<UserCommandRepositoryPort>;
     let mockPasswordCipher: jest.Mocked<PasswordCipherPort>;
     let mockPasswordValidatorService: jest.Mocked<PasswordValidatorPort>;
 
-    let userFixture: User;
-
     beforeEach(() => {
-        mockUserRepository = getUserRepositoryMocking();
+        mockUserCommandRepository = getUserCommandRepositoryMocking();
         mockPasswordCipher = getPasswordCipherMocking();
         mockPasswordValidatorService = getPasswordValidatorServiceMocking();
-
-        useCase = new ChangePasswordUseCase(
-            mockUserRepository,
-            mockPasswordCipher,
-            mockPasswordValidatorService
-        );
-
-        userFixture = User.createWithEmail(
-            Email.create('test@domain.com'),
-            Password.reconstruct('encrypted:Abcd1234@')
-        );
-
-        mockUserRepository.findByUserId.mockResolvedValue(userFixture);
 
         mockPasswordCipher.encrypt.mockImplementation(value => {
             return `encrypted:${value}`;
         });
+
+        useCase = new ChangePasswordUseCase(
+            mockUserCommandRepository,
+            mockPasswordValidatorService,
+            mockPasswordCipher
+        );
     });
 
     describe('정상 변경', () => {
         it('현재 비밀번호가 맞고, 비밀번호 유효성이 전부 통과한다면 성공한다.', async () => {
             // Give
-            mockPasswordValidatorService.passwordMatchValidate.mockReturnValue(
-                true
-            );
             mockPasswordValidatorService.passwordDifferentValidate.mockReturnValue(
                 true
             );
-            mockPasswordValidatorService.emailIncludedValidate.mockReturnValue(
-                true
-            );
+
             const dto = {
                 userId: '836397c9-06ae-4fe0-82ec-5bd7d1f22700',
                 currentPassword: 'Abcd1234@',
                 newPassword: 'Abcd1234!',
             };
-
-            mockPasswordValidatorService.emailIncludedValidate.mockReturnValue(
-                true
-            );
 
             // When
             const result = await useCase.execute(dto);
 
             // Then
             expect(
-                mockPasswordValidatorService.emailIncludedValidate
+                mockPasswordValidatorService.passwordDifferentValidate
             ).toHaveBeenCalledTimes(1);
-            expect(mockUserRepository.findByUserId).toHaveBeenCalledTimes(1);
-            expect(mockUserRepository.save).toHaveBeenCalledTimes(1);
+            expect(
+                mockUserCommandRepository.changePassword
+            ).toHaveBeenCalledTimes(1);
 
             expect(result).toBeTruthy();
-
-            const savedUser = mockUserRepository.save.mock.calls[0][0] as User;
-
-            expect(savedUser.password?.toString()).toEqual(
-                `encrypted:${dto.newPassword}`
-            );
         });
     });
 
     describe('변경 실패', () => {
-        it('현재 비밀번호가 같지 않을 경우 에러를 던진다.', async () => {
+        it('바꾸려하는 비밀번호와 현재 비밀번호가 같을 경우 에러가 발생한다.', async () => {
             // Give
-            mockPasswordValidatorService.passwordMatchValidate.mockReturnValue(
+            mockPasswordValidatorService.passwordDifferentValidate.mockReturnValue(
                 false
             );
             const dto = {
                 userId: '836397c9-06ae-4fe0-82ec-5bd7d1f22700',
-                currentPassword: 'Wrong1234@',
+                currentPassword: 'Abcd1234!',
                 newPassword: 'Abcd1234!',
             };
 
@@ -105,69 +75,12 @@ describe('ChangePasswordUseCase', () => {
             await expect(useCase.execute(dto)).rejects.toThrow(
                 ChangePasswordException
             );
-            expect(mockUserRepository.findByUserId).toHaveBeenCalledTimes(1);
-        });
-
-        it('현재 비밀번호가 같지만, 비밀번호 유효성 정책(이메일과 같은 경우) 에 실패했을 경우 에러를 던진다.', async () => {
-            // Give
-            mockPasswordValidatorService.passwordMatchValidate.mockReturnValue(
-                true
-            );
-            mockPasswordValidatorService.passwordDifferentValidate.mockReturnValue(
-                true
-            );
-            mockPasswordValidatorService.emailIncludedValidate.mockReturnValue(
-                false
-            );
-            const dto = {
-                userId: '836397c9-06ae-4fe0-82ec-5bd7d1f22700',
-                currentPassword: 'Abcd1234@',
-                newPassword: 'Test1234!',
-            };
-
-            // When & Then
-            await expect(useCase.execute(dto)).rejects.toThrow(
-                InvalidPasswordException
-            );
             expect(
-                mockPasswordValidatorService.emailIncludedValidate
+                mockPasswordValidatorService.passwordDifferentValidate
             ).toHaveBeenCalledTimes(1);
-            expect(mockUserRepository.findByUserId).toHaveBeenCalledTimes(1);
-        });
-
-        it('현재 비밀번호가 같지만, 비밀번호 유효성 정책(대소문자, 특문 등) 에 실패했을 경우 에러를 던진다.', async () => {
-            // Give
-            const dto = {
-                userId: '836397c9-06ae-4fe0-82ec-5bd7d1f22700',
-                currentPassword: 'Abcd1234@',
-                newPassword: 'test1234!',
-            };
-
-            // When & Then
-            expect(() => {
-                ChangePasswordRequestSchema.parse(dto);
-            }).toThrow(InvalidPasswordException);
-        });
-
-        it('현재 비밀번호와 변경하려고 하는 비밀번호가 같은 경우 에러를 던진다.', async () => {
-            // Give
-            mockPasswordValidatorService.emailIncludedValidate.mockReturnValue(
-                true
-            );
-            mockPasswordValidatorService.passwordDifferentValidate.mockReturnValue(
-                false
-            );
-            const dto = {
-                userId: '836397c9-06ae-4fe0-82ec-5bd7d1f22700',
-                currentPassword: 'Abcd1234@',
-                newPassword: 'Abcd1234@',
-            };
-
-            // When & Then
-            await expect(useCase.execute(dto)).rejects.toThrow(
-                ChangePasswordException
-            );
-            expect(mockUserRepository.findByUserId).toHaveBeenCalledTimes(1);
+            expect(
+                mockUserCommandRepository.changePassword
+            ).toHaveBeenCalledTimes(0);
         });
 
         it('Use Case 내 도메인 호출 과정에서 예외가 발생하면, 예외가 그대로 전파되어야 한다.', async () => {
@@ -178,7 +91,9 @@ describe('ChangePasswordUseCase', () => {
                 newPassword: 'test1234!',
             };
 
-            mockUserRepository.findByUserId.mockRejectedValue(new Error());
+            mockUserCommandRepository.changePassword.mockRejectedValue(
+                new Error()
+            );
 
             // When & Then
             await expect(useCase.execute(dto)).rejects.toThrow(Error);
--- app/__tests__/application/user/use-cases/withdrawal.usecase.spec.ts
+++ app/__tests__/application/user/use-cases/withdrawal.usecase.spec.ts
@@ -1,86 +1,50 @@
-import { UserRepositoryPort } from '@domain/user/port/out/user-repository.port';
 import { WithdrawalUseCase } from '@/application/user/use-cases/withdrawal.usecase';
-import { AuthenticationServicePort } from '@domain/user/port/out/authentication-service.port';
-import {
-    LogoutException,
-    WithdrawalException,
-} from '@domain/user/exceptions/user.exceptions';
-import { getAuthenticationServiceMocking } from '@/__tests__/application/helpers/service-mocking.helpers';
-import { getUserRepositoryMocking } from '@/__tests__/application/helpers/repository-mocking.helpers';
+import { WithdrawalException } from '@domain/user/exceptions/user.exceptions';
+import { getUserCommandRepositoryMocking } from '@/__tests__/application/helpers/repository-mocking.helpers';
+import { UserCommandRepositoryPort } from '@domain/user/port/repositories/user-command-repository.port';
 
 describe('WithdrawalUseCase', () => {
     let useCase: WithdrawalUseCase;
-    let mockUserRepository: jest.Mocked<UserRepositoryPort>;
-    let mockAuthenticationService: jest.Mocked<AuthenticationServicePort>;
+    let mockUserCommandRepository: jest.Mocked<UserCommandRepositoryPort>;
 
     beforeEach(() => {
-        mockUserRepository = getUserRepositoryMocking();
+        mockUserCommandRepository = getUserCommandRepositoryMocking();
 
-        mockAuthenticationService = getAuthenticationServiceMocking();
-
-        useCase = new WithdrawalUseCase(
-            mockUserRepository,
-            mockAuthenticationService
-        );
+        useCase = new WithdrawalUseCase(mockUserCommandRepository);
     });
 
     describe('성공 테스트', () => {
-        it('로그아웃도 성공하고, 회원탈퇴도 성공하면 True를 반환한다.', async () => {
+        it('회원탈퇴가 성공하면 True를 반환한다.', async () => {
             // Given
-            const dto = {
-                id: '550e8400-e29b-41d4-a716-446655440000',
-            };
-
-            mockAuthenticationService.logout.mockResolvedValue(true);
+            mockUserCommandRepository.withdrawal.mockResolvedValue(true);
 
             // When
-            const result = await useCase.execute(dto);
+            const result = await useCase.execute();
 
             // Then
-            expect(mockAuthenticationService.logout).toHaveBeenCalledTimes(1);
-            expect(mockUserRepository.delete).toHaveBeenCalledTimes(1);
+            expect(mockUserCommandRepository.withdrawal).toHaveBeenCalledTimes(
+                1
+            );
 
             expect(result).toBeTruthy();
         });
     });
 
     describe('실패 테스트', () => {
-        it('로그아웃이 실패하면 에러를 던진다.', async () => {
-            // Given
-            const dto = {
-                id: '550e8400-e29b-41d4-a716-446655440000',
-            };
-
-            mockAuthenticationService.logout.mockRejectedValue(
-                new LogoutException('')
-            );
-
-            // When & Then
-            await expect(useCase.execute(dto)).rejects.toThrow(LogoutException);
-
-            expect(mockAuthenticationService.logout).toHaveBeenCalledTimes(1);
-            expect(mockUserRepository.delete).toHaveBeenCalledTimes(0);
-        });
-
         it('회원탈퇴에 실패하면 에러를 던진다.', async () => {
             // Given
-            const dto = {
-                id: '550e8400-e29b-41d4-a716-446655440000',
-            };
-
-            mockAuthenticationService.logout.mockResolvedValue(true);
-
-            mockUserRepository.delete.mockRejectedValue(
+            mockUserCommandRepository.withdrawal.mockRejectedValue(
                 new WithdrawalException('')
             );
 
             // When & Then
-            await expect(useCase.execute(dto)).rejects.toThrow(
+            await expect(useCase.execute()).rejects.toThrow(
                 WithdrawalException
             );
 
-            expect(mockAuthenticationService.logout).toHaveBeenCalledTimes(1);
-            expect(mockUserRepository.delete).toHaveBeenCalledTimes(1);
+            expect(mockUserCommandRepository.withdrawal).toHaveBeenCalledTimes(
+                1
+            );
         });
     });
 });
