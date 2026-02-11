3860c595a032baec1ca7e2656a5cb1aa0f888593
y0ngha <dev.y0ngha@gmail.com>
2026-02-11 23:44:05 +0900

    feat: 이메일, 구글 회원가입 repository 함수 생성.

--- app/domain/user/port/repositories/user-command-repository.port.ts
+++ app/domain/user/port/repositories/user-command-repository.port.ts
@@ -1,6 +1,16 @@
 import { ChangePasswordCommand } from '@domain/user/commands/change-password.command';
 import { User } from '@domain/user/models/user.model';
+import { RegisterWithEmailCommand } from '@domain/user/commands/register-with-email.command';
+import { RegisterWithGoogleCommand } from '@domain/user/commands/register-with-google.command';
 
 export abstract class UserCommandRepositoryPort {
     abstract changePassword(command: ChangePasswordCommand): Promise<User>;
+
+    abstract registerWithEmail(
+        command: RegisterWithEmailCommand
+    ): Promise<User>;
+
+    abstract registerWithGoogle(
+        command: RegisterWithGoogleCommand
+    ): Promise<User>;
 }
