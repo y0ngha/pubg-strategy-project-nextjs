957728fa3073ca0d68fc31776e356481bd38fe11
y0ngha <dev.y0ngha@gmail.com>
2026-02-12 00:10:55 +0900

    fix: 탈퇴 행위 Command Adapter에 추가.

--- app/infrastructure/user/adapter/repositories/user-command-repository.adapter.ts
+++ app/infrastructure/user/adapter/repositories/user-command-repository.adapter.ts
@@ -3,21 +3,22 @@ import { ChangePasswordCommand } from '@domain/user/commands/change-password.com
 import { User } from '@domain/user/models/user.model';
 import { RegisterWithEmailCommand } from '@domain/user/commands/register-with-email.command';
 import { RegisterWithGoogleCommand } from '@domain/user/commands/register-with-google.command';
+import { WithdrawalCommand } from '@domain/user/commands/withdrawal.command';
 
 export class UserCommandRepositoryAdapter extends UserCommandRepositoryPort {
     async changePassword(command: ChangePasswordCommand): Promise<User> {
-        // TODO 나중에 보낼 때 여기서 password encrypt 후 보내도록 해야함.
-
         throw new Error('Method not implemented.');
     }
 
     registerWithEmail(command: RegisterWithEmailCommand): Promise<User> {
-        // TODO 나중에 보낼 때 여기서 password encrypt 후 보내도록 해야함.
-
         throw new Error('Method not implemented.');
     }
 
     registerWithGoogle(command: RegisterWithGoogleCommand): Promise<User> {
+        throw new Error('Method not implemented.');
+    }
+
+    withdrawal(command: WithdrawalCommand): Promise<boolean> {
         throw new Error('Method not implemented.');
     }
 }
