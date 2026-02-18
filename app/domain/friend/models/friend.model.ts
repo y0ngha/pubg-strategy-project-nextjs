d3af425d5497b5c51efb10901246b425708fe2ef
y0ngha <dev.y0ngha@gmail.com>
2026-02-18 21:00:46 +0900

    refactor: 친구 모델에서 status는 FriendStatus Enum 가져가도록 수정.

--- app/domain/friend/models/friend.model.ts
+++ app/domain/friend/models/friend.model.ts
@@ -1,8 +1,10 @@
+import { FriendStatus } from '@domain/friend/enum/friend-status.enum';
+
 export interface Friend {
     readonly id: string;
     readonly requesterUserId: string;
     readonly recipientUserId: string;
-    readonly status: string;
+    readonly status: FriendStatus;
     readonly requesterUserEmail: string;
     readonly recipientUserEmail: string;
     readonly requestedAt: Date;
