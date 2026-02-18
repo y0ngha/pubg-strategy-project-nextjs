e21af85cbfa3105af472781cbdcd374e816d7ecf
y0ngha <dev.y0ngha@gmail.com>
2026-02-18 21:01:12 +0900

    refactor: Friend Query Repository에서 조회시 userId 넘기던 부분 삭제.

--- app/domain/friend/port/repositories/friend-query-repository.port.ts
+++ app/domain/friend/port/repositories/friend-query-repository.port.ts
@@ -1,22 +1,12 @@
-import { UserId } from '@domain/shared/value-objects/user-id';
 import { FriendId } from '@domain/friend/value-objects/friend-id';
 import { Friend } from '@domain/friend/models/friend.model';
 
 export abstract class FriendQueryRepositoryPort {
-    abstract existsFriendBetween(
-        userId1: UserId,
-        userId2: UserId
-    ): Promise<boolean>;
-
     abstract findById(id: FriendId): Promise<Friend | null>;
 
-    abstract findAcceptedFriendsByUserId(userId: UserId): Promise<Friend[]>;
+    abstract findAcceptedFriendsByUserId(): Promise<Friend[]>;
 
-    abstract findReceivedFriendRequestsByRecipientUserId(
-        userId: UserId
-    ): Promise<Friend[]>;
+    abstract findReceivedFriendRequestsByRecipientUserId(): Promise<Friend[]>;
 
-    abstract findSentFriendRequestsByRequesterUserId(
-        userId: UserId
-    ): Promise<Friend[]>;
+    abstract findSentFriendRequestsByRequesterUserId(): Promise<Friend[]>;
 }
--- app/infrastructure/friend/adapter/repositories/friend-query-repository.adapter.ts
+++ app/infrastructure/friend/adapter/repositories/friend-query-repository.adapter.ts
@@ -1,14 +1,9 @@
 import { FriendQueryRepositoryPort } from '@domain/friend/port/repositories/friend-query-repository.port';
 import { FriendId } from '@domain/friend/value-objects/friend-id';
-import { UserId } from '@domain/shared/value-objects/user-id';
 import { Friend } from '@domain/friend/models/friend.model';
 
 export class FriendQueryRepositoryAdapter extends FriendQueryRepositoryPort {
-    existsFriendBetween(userId1: UserId, userId2: UserId): Promise<boolean> {
-        throw new Error('Not Implemented.');
-    }
-
-    findAcceptedFriendsByUserId(userId: UserId): Promise<Friend[]> {
+    findAcceptedFriendsByUserId(): Promise<Friend[]> {
         throw new Error('Not Implemented.');
     }
 
@@ -16,13 +11,11 @@ export class FriendQueryRepositoryAdapter extends FriendQueryRepositoryPort {
         throw new Error('Not Implemented.');
     }
 
-    findReceivedFriendRequestsByRecipientUserId(
-        userId: UserId
-    ): Promise<Friend[]> {
+    findReceivedFriendRequestsByRecipientUserId(): Promise<Friend[]> {
         throw new Error('Not Implemented.');
     }
 
-    findSentFriendRequestsByRequesterUserId(userId: UserId): Promise<Friend[]> {
+    findSentFriendRequestsByRequesterUserId(): Promise<Friend[]> {
         throw new Error('Not Implemented.');
     }
 }
