905fd444091b4b081e1c4a17c07c197d65aacd4e
y0ngha <dev.y0ngha@gmail.com>
2026-02-18 20:28:18 +0900

    refactor: 친구 요청 수락 Use Case 변경된 도메인에 맞게 리팩토링.

--- app/__tests__/application/friend/use-cases/accept-received-friend.usecase.spec.ts
+++ app/__tests__/application/friend/use-cases/accept-received-friend.usecase.spec.ts
@@ -1,175 +1,73 @@
 import { AcceptReceivedFriendUseCase } from '@/application/friend/use-cases/accept-received-friend.usecase';
-import { FriendRepositoryPort } from '@domain/friend/port/out/friend-repository.port';
-import { Friend } from '@domain/friend/entities/friend.entity';
-import { UserId } from '@domain/shared/value-objects/user-id';
-import { Email } from '@domain/shared/value-objects/email';
 import { FriendStatus } from '@domain/friend/enum/friend-status.enum';
-import {
-    FriendNotFoundException,
-    FriendUpdateInvalidPermission,
-    FriendUpdateInvalidStatus,
-} from '@domain/friend/exceptions/friend.exceptions';
-import { getFriendRepositoryMocking } from '@/__tests__/application/helpers/repository-mocking.helpers';
+import { FriendUpdateInvalidStatus } from '@domain/friend/exceptions/friend.exceptions';
+import { getFriendCommandRepositoryMocking } from '@/__tests__/application/helpers/repository-mocking.helpers';
+import { FriendCommandRepositoryPort } from '@domain/friend/port/repositories/friend-command-repository.port';
 
 describe('AcceptReceivedFriendUseCase', () => {
     let useCase: AcceptReceivedFriendUseCase;
-    let mockFriendRepository: jest.Mocked<FriendRepositoryPort>;
+    let mockFriendCommandRepository: jest.Mocked<FriendCommandRepositoryPort>;
 
-    const requesterUserId = 'a0f01e35-f96b-4dee-a75b-89cea500ce50';
-    const recipientUserId = 'c92c6abc-82aa-423f-83e9-9e2823c1bcf7';
-
-    const requesterUserEmail = 'requester@domain.com';
-    const recipientUserEmail = 'recipient@domain.com';
-
-    let friendFixture: Friend;
+    const friendId = 'c92c6abc-82aa-423f-83e9-9e2823c1bcf7';
 
     beforeEach(() => {
-        mockFriendRepository = getFriendRepositoryMocking();
+        mockFriendCommandRepository = getFriendCommandRepositoryMocking();
 
-        friendFixture = Friend.create(
-            UserId.create(requesterUserId),
-            UserId.create(recipientUserId),
-            Email.create(requesterUserEmail),
-            Email.create(recipientUserEmail)
-        );
-
-        useCase = new AcceptReceivedFriendUseCase(mockFriendRepository);
+        useCase = new AcceptReceivedFriendUseCase(mockFriendCommandRepository);
     });
 
     describe('성공 테스트', () => {
-        it('친구 관계가 존재하고, 받은 사람이 본인이며, 친구 상태가 PENDING일 때 성공한다.', async () => {
+        it('PENDING 상태라면, Command를 생성하여 Repository에 전달한다.', async () => {
             // give
-            mockFriendRepository.findById.mockResolvedValue(friendFixture);
-
+            const currentStatus = FriendStatus.PENDING;
             const dto = {
-                id: '869215ed-3baf-4abb-a511-b164f0cc716e',
-                userId: recipientUserId,
+                id: friendId,
+                currentStatus: currentStatus,
             };
 
             // when
             await useCase.execute(dto);
 
             // then
-            expect(mockFriendRepository.findById).toHaveBeenCalledTimes(1);
-            expect(mockFriendRepository.save).toHaveBeenCalledTimes(1);
-
-            const savedFriend = mockFriendRepository.save.mock.calls[0][0];
-
-            expect(savedFriend.status).toBe(FriendStatus.ACCEPTED);
-            expect(savedFriend.respondedAt).not.toBeNull();
+            expect(mockFriendCommandRepository.accept).toHaveBeenCalledTimes(1);
+            expect(mockFriendCommandRepository.accept).toHaveBeenCalledWith(
+                expect.objectContaining(dto)
+            );
         });
     });
 
     describe('실패 테스트', () => {
-        it('친구 관계가 없을 때 에러를 던진다.', async () => {
+        it('PENDING 상태가 아니라면, Command 생성 과정에서 에러가 발생하여 Repository에 전달하지도 않는다.', async () => {
             // give
-            mockFriendRepository.findById.mockResolvedValue(null);
-
+            const currentStatus = FriendStatus.CANCELED;
             const dto = {
-                id: '869215ed-3baf-4abb-a511-b164f0cc716e',
-                userId: recipientUserId,
-            };
-
-            // when & then
-            await expect(useCase.execute(dto)).rejects.toThrow(
-                FriendNotFoundException
-            );
-            expect(mockFriendRepository.findById).toHaveBeenCalledTimes(1);
-            expect(mockFriendRepository.save).toHaveBeenCalledTimes(0);
-        });
-
-        it('본인이 받은 요청이 아닌 경우 에러를 던진다.', async () => {
-            // give
-            mockFriendRepository.findById.mockResolvedValue(friendFixture);
-
-            const dto = {
-                id: '869215ed-3baf-4abb-a511-b164f0cc716e',
-                userId: requesterUserId,
-            };
-
-            // when & then
-            await expect(useCase.execute(dto)).rejects.toThrow(
-                FriendUpdateInvalidPermission
-            );
-
-            expect(mockFriendRepository.findById).toHaveBeenCalledTimes(1);
-            expect(mockFriendRepository.save).toHaveBeenCalledTimes(0);
-        });
-
-        it('요청 받은 친구 관계가 이미 수락된 상태인 경우 에러를 던진다.', async () => {
-            // give
-            friendFixture.accept(UserId.create(recipientUserId));
-            mockFriendRepository.findById.mockResolvedValue(friendFixture);
-
-            const dto = {
-                id: '869215ed-3baf-4abb-a511-b164f0cc716e',
-                userId: recipientUserId,
+                id: friendId,
+                currentStatus: currentStatus,
             };
 
             // when & then
             await expect(useCase.execute(dto)).rejects.toThrow(
                 FriendUpdateInvalidStatus
             );
-
-            expect(mockFriendRepository.findById).toHaveBeenCalledTimes(1);
-            expect(mockFriendRepository.save).toHaveBeenCalledTimes(0);
-        });
-
-        it('요청 받은 친구 관계가 이미 거절된 상태인 경우 에러를 던진다.', async () => {
-            // give
-            friendFixture.reject(UserId.create(recipientUserId));
-            mockFriendRepository.findById.mockResolvedValue(friendFixture);
-
-            const dto = {
-                id: '869215ed-3baf-4abb-a511-b164f0cc716e',
-                userId: recipientUserId,
-            };
-
-            // when & then
-            await expect(useCase.execute(dto)).rejects.toThrow(
-                FriendUpdateInvalidStatus
-            );
-
-            expect(mockFriendRepository.findById).toHaveBeenCalledTimes(1);
-            expect(mockFriendRepository.save).toHaveBeenCalledTimes(0);
-        });
-
-        it('요청 받은 친구 관계가 취소된 상태인 경우 에러를 던진다.', async () => {
-            // give
-            friendFixture.cancel(UserId.create(requesterUserId));
-            mockFriendRepository.findById.mockResolvedValue(friendFixture);
-
-            const dto = {
-                id: '869215ed-3baf-4abb-a511-b164f0cc716e',
-                userId: recipientUserId,
-            };
-
-            // when & then
-            await expect(useCase.execute(dto)).rejects.toThrow(
-                FriendUpdateInvalidStatus
-            );
-
-            expect(mockFriendRepository.findById).toHaveBeenCalledTimes(1);
-            expect(mockFriendRepository.save).toHaveBeenCalledTimes(0);
+            expect(mockFriendCommandRepository.accept).toHaveBeenCalledTimes(0);
         });
     });
 
     it('Use Case 내 도메인 호출 과정에서 예외가 발생하면, 예외가 그대로 전파되어야 한다.', async () => {
         // given
-        jest.spyOn(friendFixture, 'accept').mockImplementation(() => {
-            throw new FriendUpdateInvalidPermission();
-        });
+        jest.spyOn(mockFriendCommandRepository, 'accept').mockImplementation(
+            () => {
+                throw new Error();
+            }
+        );
 
-        mockFriendRepository.findById.mockResolvedValue(friendFixture);
-
+        const currentStatus = FriendStatus.PENDING;
         const dto = {
-            id: '869215ed-3baf-4abb-a511-b164f0cc716e',
-            userId: recipientUserId,
+            id: friendId,
+            currentStatus: currentStatus,
         };
 
         //when & then
-        await expect(() => useCase.execute(dto)).rejects.toThrow(
-            FriendUpdateInvalidPermission
-        );
+        await expect(() => useCase.execute(dto)).rejects.toThrow(Error);
     });
 });
--- app/application/friend/use-cases/accept-received-friend.usecase.ts
+++ app/application/friend/use-cases/accept-received-friend.usecase.ts
@@ -1,30 +1,25 @@
 import { inject, injectable } from 'inversify';
-import { FriendRepositoryPort } from '@domain/friend/port/out/friend-repository.port';
 import {
     AcceptReceivedFriendRequestDto,
     AcceptReceivedFriendRequestSchema,
 } from '@/application/friend/dto/accept-received-friend.dto';
-import { FriendNotFoundException } from '@domain/friend/exceptions/friend.exceptions';
+import { FriendCommandRepositoryPort } from '@domain/friend/port/repositories/friend-command-repository.port';
+import { AcceptFriendRequestCommand } from '@domain/friend/commands/accept-friend-request.command';
 
 @injectable()
 export class AcceptReceivedFriendUseCase {
     constructor(
-        @inject(FriendRepositoryPort)
-        private readonly friendRepository: FriendRepositoryPort
+        @inject(FriendCommandRepositoryPort)
+        private readonly friendCommandRepository: FriendCommandRepositoryPort
     ) {}
 
     async execute(dto: AcceptReceivedFriendRequestDto): Promise<boolean> {
-        const { id, userId } = AcceptReceivedFriendRequestSchema.parse(dto);
+        const { id, currentStatus } =
+            AcceptReceivedFriendRequestSchema.parse(dto);
 
-        const friend = await this.friendRepository.findById(id);
+        const command = AcceptFriendRequestCommand.create(id, currentStatus);
 
-        if (!friend) {
-            throw new FriendNotFoundException(id.toString());
-        }
-
-        friend.accept(userId);
-
-        await this.friendRepository.save(friend);
+        await this.friendCommandRepository.accept(command);
 
         return true;
     }
