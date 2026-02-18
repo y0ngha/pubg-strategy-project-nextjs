d390c8924d6663abc1c999983087034f85c54028
y0ngha <dev.y0ngha@gmail.com>
2026-02-18 20:37:05 +0900

    fix: 친구 요청 취소 Use Case에서 AcceptReceivedFriendRequestSchema 참조하던 부분 수정.

--- app/application/friend/use-cases/cancel-sent-friend.usecase.ts
+++ app/application/friend/use-cases/cancel-sent-friend.usecase.ts
@@ -1,7 +1,9 @@
 import { inject, injectable } from 'inversify';
-import { CancelSentFriendRequestDto } from '@/application/friend/dto/cancel-sent-friend.dto';
+import {
+    CancelSentFriendRequestDto,
+    CancelSentFriendRequestSchema,
+} from '@/application/friend/dto/cancel-sent-friend.dto';
 import { FriendCommandRepositoryPort } from '@domain/friend/port/repositories/friend-command-repository.port';
-import { AcceptReceivedFriendRequestSchema } from '@/application/friend/dto/accept-received-friend.dto';
 import { CancelSentFriendRequestCommand } from '@domain/friend/commands/cancel-sent-friend-request.command';
 
 @injectable()
@@ -12,8 +14,7 @@ export class CancelSentFriendUseCase {
     ) {}
 
     async execute(dto: CancelSentFriendRequestDto): Promise<boolean> {
-        const { id, currentStatus } =
-            AcceptReceivedFriendRequestSchema.parse(dto);
+        const { id, currentStatus } = CancelSentFriendRequestSchema.parse(dto);
 
         const command = CancelSentFriendRequestCommand.create(
             id,
