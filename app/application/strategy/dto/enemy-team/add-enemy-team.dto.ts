5d0c30475c243dd6bb5a49e4ea7793383b414b9f
y0ngha <dev.y0ngha@gmail.com>
2026-02-19 03:06:00 +0900

    refactor: 적 팀 생성 Use Case 변경된 Domain에 맞게 재구성.

--- app/application/strategy/dto/enemy-team/add-enemy-team.dto.ts
+++ app/application/strategy/dto/enemy-team/add-enemy-team.dto.ts
@@ -1,21 +1,16 @@
 import { z } from 'zod';
-import { UserId } from '@domain/shared/value-objects/user-id';
 import { StrategyId } from '@domain/strategy/value-objects/strategy-id';
 import { TeamLabel } from '@domain/strategy/value-objects/team-label';
 import { Position as PositionInterface } from '@/application/strategy/types/position';
 import { Position } from '@domain/strategy/value-objects/position';
 
 export interface AddEnemyTeamRequestDto {
-    actorId: string;
     strategyId: string;
     teamLabel: string;
     position: PositionInterface;
 }
 
 export const AddEnemyTeamRequestSchema = z.object({
-    actorId: z.string().transform(value => {
-        return UserId.create(value);
-    }),
     strategyId: z.string().transform(value => {
         return StrategyId.create(value);
     }),
