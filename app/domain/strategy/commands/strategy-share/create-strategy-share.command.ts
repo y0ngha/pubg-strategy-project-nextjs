8c8a633ebad4af0c0680c9d4fd0aad453d9c7d4c
y0ngha <dev.y0ngha@gmail.com>
2026-02-19 23:20:19 +0900

    feat: 전략 공유 권한중 ACCESS_DENIED 삭제. - 원래는 Class 내에서 상태를 관리하기 위한 목적으로 생겨난 권한값으로, 이젠 제거.

--- app/domain/strategy/commands/strategy-share/create-strategy-share.command.ts
+++ app/domain/strategy/commands/strategy-share/create-strategy-share.command.ts
@@ -2,19 +2,17 @@ import { StrategyId } from '@domain/strategy/value-objects/strategy-id';
 import { UserId } from '@domain/shared/value-objects/user-id';
 import { StrategySharePermission } from '@domain/strategy/enums/strategy-share-permission.enum';
 
-type AvaliablePermission = Exclude<StrategySharePermission, 'ACCESS_DENIED'>;
-
 export class CreateStrategyShareCommand {
     private constructor(
         public readonly strategyId: StrategyId,
         public readonly targetUserId: UserId,
-        public readonly permission: AvaliablePermission
+        public readonly permission: StrategySharePermission
     ) {}
 
     static create(
         strategyId: StrategyId,
         targetUserId: UserId,
-        permission: AvaliablePermission
+        permission: StrategySharePermission
     ) {
         return new CreateStrategyShareCommand(
             strategyId,
--- app/domain/strategy/commands/strategy-share/update-strategy-share-permission.command.ts
+++ app/domain/strategy/commands/strategy-share/update-strategy-share-permission.command.ts
@@ -2,19 +2,17 @@ import { StrategyId } from '@domain/strategy/value-objects/strategy-id';
 import { StrategySharePermission } from '@domain/strategy/enums/strategy-share-permission.enum';
 import { StrategyShareId } from '@domain/strategy/value-objects/strategy-share-id';
 
-type AvaliablePermission = Exclude<StrategySharePermission, 'ACCESS_DENIED'>;
-
 export class UpdateStrategySharePermissionCommand {
     private constructor(
         public readonly strategyId: StrategyId,
         public readonly strategyShareId: StrategyShareId,
-        public readonly permission: AvaliablePermission
+        public readonly permission: StrategySharePermission
     ) {}
 
     static create(
         strategyId: StrategyId,
         strategyShareId: StrategyShareId,
-        permission: AvaliablePermission
+        permission: StrategySharePermission
     ) {
         return new UpdateStrategySharePermissionCommand(
             strategyId,
