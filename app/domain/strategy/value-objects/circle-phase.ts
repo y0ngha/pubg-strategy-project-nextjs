9bddb1e3c7135c313a4ca1372a2fa1915d384b02
y0ngha <dev.y0ngha@gmail.com>
2026-02-19 18:49:44 +0900

    refactor: circle-phase VO에서 radius, color 가져올 수 있는 메서드 삭제.

--- app/domain/strategy/value-objects/circle-phase.ts
+++ app/domain/strategy/value-objects/circle-phase.ts
@@ -1,22 +1,11 @@
 import { InvalidCirclePhaseException } from '@domain/strategy/exceptions/strategy.exceptions';
-import { CircleColor } from '@domain/strategy/enums/circle-color.enum';
 import {
-    CIRCLE_COLOR_MAP,
     CIRCLE_MAX_PHASE,
     CIRCLE_MIN_PHASE,
-    CIRCLE_RADIUS_MAP,
 } from '@domain/strategy/constants/circle-phase.constants';
 
 export class CirclePhase {
     constructor(public readonly value: number) {}
-
-    get radius(): number {
-        return CIRCLE_RADIUS_MAP[this.value];
-    }
-
-    get color(): CircleColor {
-        return CIRCLE_COLOR_MAP[this.value];
-    }
 
     static create(value: number) {
         CirclePhase.ensureValidPhase(value);
