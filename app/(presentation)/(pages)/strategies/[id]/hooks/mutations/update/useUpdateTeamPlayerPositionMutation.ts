8912322e84a3f10f4d8381bd5a808d51532aae64
y0ngha <dev.y0ngha@gmail.com>
2026-02-21 19:26:53 +0900

    chore: useUpdateTeamPlayerPositionMutation에서 실패할 경우 나오는 콘솔 에러 로그 함수명 반영 안된 부분 수정.

--- app/(presentation)/(pages)/strategies/[id]/hooks/mutations/update/useUpdateTeamPlayerPositionMutation.ts
+++ app/(presentation)/(pages)/strategies/[id]/hooks/mutations/update/useUpdateTeamPlayerPositionMutation.ts
@@ -39,7 +39,7 @@ export function useUpdateTeamPlayerPositionMutation(strategyId: string) {
                 queryKey: strategyQueryKey,
             });
 
-            console.error('useUpdateTeamPlayerMutation', error);
+            console.error('useUpdateTeamPlayerPositionMutation', error);
             toast.error(
                 error.message ??
                     '알 수 없는 오류로 팀 플레이어 수정에 실패했습니다.'
