298cba302c5870cf21f4e3174e0e37e7c39c5458
y0ngha <dev.y0ngha@gmail.com>
2026-02-21 18:24:46 +0900

    fix: 인증 서비스 어댑터에서 파라미터로 Command가 아니라 개별 파라미터로 받고 있던 부분 수정.

--- app/infrastructure/user/adapter/driven/authentication-service.adapter.ts
+++ app/infrastructure/user/adapter/driven/authentication-service.adapter.ts
@@ -1,24 +1,23 @@
-import { Email } from '@/domain/shared/value-objects/email';
 import { AuthenticationServicePort } from '@/domain/user/port/out/authentication-service.port';
-import { Password } from '@/domain/user/value-objects/password';
 import { LogoutCommand } from '@domain/user/commands/logout.command';
+import { LoginWithEmailCommand } from '@domain/user/commands/login-with-email.command';
+import { LoginWithGoogleCommand } from '@domain/user/commands/login-with-google.command';
 
 export class AuthenticationServiceAdapter extends AuthenticationServicePort {
-    async login(
-        email: Email,
-        password: Password
+    googleLogin(command: LoginWithGoogleCommand): Promise<{
+        accessToken: string;
+        refreshToken: string;
+    }> {
+        throw new Error('Not Implemented.');
+    }
+
+    login(
+        command: LoginWithEmailCommand
     ): Promise<{ accessToken: string; refreshToken: string }> {
         throw new Error('Not Implemented.');
     }
 
     async logout(command: LogoutCommand): Promise<boolean> {
-        throw new Error('Not Implemented.');
-    }
-
-    async googleLogin(
-        email: Email,
-        token: string
-    ): Promise<{ accessToken: string; refreshToken: string }> {
         throw new Error('Not Implemented.');
     }
 }
