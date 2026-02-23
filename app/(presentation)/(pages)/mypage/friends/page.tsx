dee0706eb8baa14e70bca55bd16dac582994ce81
y0ngha <dev.y0ngha@gmail.com>
2026-02-23 22:39:23 +0900

    feat: 친구 관리 페이지 작성.

---
+++ app/(presentation)/(pages)/mypage/friends/page.tsx
@@ -0,0 +1,15 @@
+import FriendsPageNavigation from '@/(presentation)/(pages)/mypage/friends/components/friends-page-navigation.component';
+import FriendsPageHeader from '@/(presentation)/(pages)/mypage/friends/components/friends-page-header.component';
+import FriendsPageFindNewFriend from '@/(presentation)/(pages)/mypage/friends/components/friends-page-find-new-friend.component';
+import FriendsPageBody from '@/(presentation)/(pages)/mypage/friends/components/friends-page-body.component';
+
+export default function FriendsPage() {
+    return (
+        <div className={'mx-auto max-w-3xl space-y-6 px-4 py-10'}>
+            <FriendsPageNavigation />
+            <FriendsPageHeader>친구 관리</FriendsPageHeader>
+            <FriendsPageFindNewFriend />
+            <FriendsPageBody />
+        </div>
+    );
+}
